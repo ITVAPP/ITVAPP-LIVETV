43ed087f37e6c1ff102f6d1e5c606604b653466c
电视宝 www.itvapp.net <service@itvapp.net>
2024-09-10 22:11:25 +0800

    Update MainActivity.kt

--- android/app/src/main/kotlin/itvapp_live_tv/MainActivity.kt
+++ android/app/src/main/kotlin/itvapp_live_tv/MainActivity.kt
@@ -3,18 +3,18 @@ package itvapp_live_tv
 import android.content.Context
 import android.app.UiModeManager
 import android.content.res.Configuration
-import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine // 确保导入这个类
 import io.flutter.plugin.common.MethodChannel
 
 class MainActivity : FlutterActivity() {
-    private val CHANNEL = "net.itvapp.isTV" // 修改后的 Channel 名称
+    private val CHANNEL = "net.itvapp.isTV"
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
 
         // 设置 Platform Channel
-        MethodChannel(flutterEngine?.dartExecutor?.binaryMessenger, CHANNEL).setMethodCallHandler { call, result ->
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler { call, result ->
             if (call.method == "isTV") {
                 val isTV = isTV() // 调用 isTV 方法判断设备是否为电视
                 result.success(isTV)
