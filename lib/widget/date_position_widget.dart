4c5c0c971830ce2a2a4e83cf80a74a2a8926ea45
电视宝 www.itvapp.net <service@itvapp.net>
2024-09-10 20:35:20 +0800

    Update date_position_widget.dart

--- lib/widget/date_position_widget.dart
+++ lib/widget/date_position_widget.dart
@@ -1,65 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';  // 导入 Provider 包
+import '../provider/theme_provider.dart'; // 导入 ThemeProvider
 import '../util/date_util.dart';
-import '../util/env_util.dart';  // 导入 EnvUtil
 
 class DatePositionWidget extends StatelessWidget {
   const DatePositionWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<bool>(
-      future: EnvUtil.isTV(), // 异步调用 isTV 方法
-      builder: (context, snapshot) {
-        if (!snapshot.hasData) {
-          return SizedBox.shrink(); // 如果没有数据，显示空白或占位内容
-        }
+    // 通过 Provider 获取 isTV 的状态
+    bool isTV = context.watch<ThemeProvider>().isTV;
 
-        // 使用 EnvUtil 来判断是否是电视
-        bool isTV = snapshot.data!; // 获取异步返回的结果
-
-        return Positioned(
-          top: isTV ? 20 : 12, // 电视上距离顶部更远
-          right: isTV ? 20 : 12, // 电视上距离右侧更远
-          child: IgnorePointer(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                // 第一行显示日期和星期
-                Text(
-                  "${DateUtil.formatDate(DateTime.now(), format: 'yyyy/MM/dd')} ${DateUtil.getWeekday(DateTime.now(), languageCode: 'zh')}",
-                  style: TextStyle(
-                    fontSize: isTV ? 22 : 16, // 电视上日期字体更大
-                    color: Colors.white70,
-                    shadows: const [
-                      Shadow(
-                        blurRadius: 8.0,
-                        color: Colors.black45,
-                        offset: Offset(0, 2),
-                      ),
-                    ],
+    return Positioned(
+      top: isTV ? 20 : 12, // 电视上距离顶部更远
+      right: isTV ? 20 : 12, // 电视上距离右侧更远
+      child: IgnorePointer(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            // 第一行显示日期和星期
+            Text(
+              "${DateUtil.formatDate(DateTime.now(), format: 'yyyy/MM/dd')} ${DateUtil.getWeekday(DateTime.now(), languageCode: 'zh')}",
+              style: TextStyle(
+                fontSize: isTV ? 22 : 16, // 电视上日期字体更大
+                color: Colors.white70,
+                shadows: const [
+                  Shadow(
+                    blurRadius: 8.0,
+                    color: Colors.black45,
+                    offset: Offset(0, 2),
                   ),
-                ),
-                // 第二行显示时间
-                Text(
-                  DateUtil.formatDate(DateTime.now(), format: 'HH:mm'),
-                  style: TextStyle(
-                    fontSize: isTV ? 50 : 38, // 电视上时间字体更大
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                    shadows: const [
-                      Shadow(
-                        blurRadius: 10.0, // 模糊效果
-                        color: Colors.black,
-                        offset: Offset(0, 2),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-        );
-      }
+            // 第二行显示时间
+            Text(
+              DateUtil.formatDate(DateTime.now(), format: 'HH:mm'),
+              style: TextStyle(
+                fontSize: isTV ? 50 : 38, // 电视上时间字体更大
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                shadows: const [
+                  Shadow(
+                    blurRadius: 10.0, // 模糊效果
+                    color: Colors.black,
+                    offset: Offset(0, 2),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
