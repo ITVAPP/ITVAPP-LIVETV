8ea29a48df5cd95708c7feaddb455a57dc0e970a
电视宝 www.itvapp.net <service@itvapp.net>
2024-09-11 23:01:09 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -28,60 +28,62 @@ import 'setting/setting_page.dart';
 void main() async {
   // 确保 WidgetsFlutterBinding 已经初始化，必要时会为应用的生命周期提供必要的绑定。
   WidgetsFlutterBinding.ensureInitialized();
+
+  LogUtil.safeExecute(() async {
+    // 如果当前环境不是移动端
+    if (!EnvUtil.isMobile) {
+      // 初始化窗口管理器（用于桌面端窗口管理）
+      await windowManager.ensureInitialized();
+      
+      // 设置窗口的选项，如窗口大小、最小大小、是否居中、背景透明等
+      WindowOptions windowOptions = const WindowOptions(
+        size: Size(414, 414 * 16 / 9),  // 窗口初始大小
+        minimumSize: Size(300, 300 * 9 / 16),  // 窗口最小大小
+        center: true,  // 窗口居中显示
+        backgroundColor: Colors.transparent,  // 背景透明
+        skipTaskbar: false,  // 不从任务栏隐藏
+        titleBarStyle: TitleBarStyle.hidden,  // 隐藏标题栏
+        title: 'ITVAPP LIVETV',  // 窗口标题
+      );
+      
+      // 等待窗口准备好后展示并聚焦
+      windowManager.waitUntilReadyToShow(windowOptions, () async {
+        await windowManager.show();  // 显示窗口
+        await windowManager.focus();  // 聚焦窗口
+      });
+    }
   
-  // 如果当前环境不是移动端
-  if (!EnvUtil.isMobile) {
-    // 初始化窗口管理器（用于桌面端窗口管理）
-    await windowManager.ensureInitialized();
+    // 启用 WakelockPlus 以防止屏幕锁定（用于移动设备）
+    WakelockPlus.enable();
     
-    // 设置窗口的选项，如窗口大小、最小大小、是否居中、背景透明等
-    WindowOptions windowOptions = const WindowOptions(
-      size: Size(414, 414 * 16 / 9),  // 窗口初始大小
-      minimumSize: Size(300, 300 * 9 / 16),  // 窗口最小大小
-      center: true,  // 窗口居中显示
-      backgroundColor: Colors.transparent,  // 背景透明
-      skipTaskbar: false,  // 不从任务栏隐藏
-      titleBarStyle: TitleBarStyle.hidden,  // 隐藏标题栏
-      title: 'ITVAPP LIVETV',  // 窗口标题
-    );
+    // 初始化共享存储的工具实例，用于管理存储操作
+    await SpUtil.getInstance();
     
-    // 等待窗口准备好后展示并聚焦
-    windowManager.waitUntilReadyToShow(windowOptions, () async {
-      await windowManager.show();  // 显示窗口
-      await windowManager.focus();  // 聚焦窗口
+    // 注册 FVP 播放器，支持不同平台和解码器
+    fvp.registerWith(options: {
+      'platforms': ['android', 'ios'],  // 支持的平台
+      'video.decoders': ['FFmpeg']  // 使用 FFmpeg 进行视频解码
     });
-  }
 
-  // 启用 WakelockPlus 以防止屏幕锁定（用于移动设备）
-  WakelockPlus.enable();
-  
-  // 初始化共享存储的工具实例，用于管理存储操作
-  await SpUtil.getInstance();
-  
-  // 注册 FVP 播放器，支持不同平台和解码器
-  fvp.registerWith(options: {
-    'platforms': ['android', 'ios'],  // 支持的平台
-    'video.decoders': ['FFmpeg']  // 使用 FFmpeg 进行视频解码
-  });
+    // 运行应用，并使用 MultiProvider 来进行全局状态管理
+    runApp(MultiProvider(
+      providers: [
+        // 状态管理：主题提供者
+        ChangeNotifierProvider(create: (_) => ThemeProvider()), 
+        // 状态管理：下载管理提供者
+        ChangeNotifierProvider(create: (_) => DownloadProvider()), 
+        // 状态管理：语言提供者
+        ChangeNotifierProvider(create: (_) => LanguageProvider()), 
+      ],
+      // 指定应用的根 widget 为 MyApp
+      child: const MyApp(),
+    ));
 
-  // 运行应用，并使用 MultiProvider 来进行全局状态管理
-  runApp(MultiProvider(
-    providers: [
-      // 状态管理：主题提供者
-      ChangeNotifierProvider(create: (_) => ThemeProvider()), 
-      // 状态管理：下载管理提供者
-      ChangeNotifierProvider(create: (_) => DownloadProvider()), 
-      // 状态管理：语言提供者
-      ChangeNotifierProvider(create: (_) => LanguageProvider()), 
-    ],
-    // 指定应用的根 widget 为 MyApp
-    child: const MyApp(),
-  ));
-
-  // 如果当前平台是 Android，设置状态栏为透明
-  if (Platform.isAndroid) {
-    SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(statusBarColor: Colors.transparent));
-  }
+    // 如果当前平台是 Android，设置状态栏为透明
+    if (Platform.isAndroid) {
+      SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(statusBarColor: Colors.transparent));
+    }
+  }, '应用初始化时发生错误');
 }
 
 // 应用的主界面
@@ -103,109 +105,115 @@ class MyApp extends StatelessWidget {
           fontFamily = null;
         }
 
-        // 返回 MaterialApp，配置应用的主题、语言、路由等
-        return MaterialApp(
-          title: 'ITVAPP LIVETV',  // 应用标题
-          
-          // 设置应用的主题，包括亮度、颜色方案、字体和其他 UI 样式
-          theme: ThemeData(
-              brightness: Brightness.dark,  // 使用暗色主题
-              fontFamily: fontFamily,  // 设置字体
-              colorScheme: ColorScheme.fromSeed(seedColor: Colors.redAccent, brightness: Brightness.dark),  // 基于种子颜色生成色系
-              scaffoldBackgroundColor: Colors.black,  // 背景色设为黑色
-              appBarTheme: const AppBarTheme(
-                backgroundColor: Colors.black,  // AppBar 背景色
-                foregroundColor: Colors.white,  // AppBar 文字颜色
-                elevation: 0,  // 移除阴影
-                centerTitle: true,  // 标题居中
-              ),
-              useMaterial3: true),  // 使用 Material Design 3
-
-          // 设置应用的语言
-          locale: languageProvider.currentLocale,  
-
-          // 定义路由配置
-          routes: {
-            RouterKeys.subScribe: (BuildContext context) => const SubScribePage(),  // 订阅页面
-            RouterKeys.setting: (BuildContext context) => const SettingPage(),  // 设置页面
-            RouterKeys.settingFont: (BuildContext context) => const SettingFontPage(),  // 字体设置页面
-            RouterKeys.settingBeautify: (BuildContext context) => const SettingBeautifyPage(),  // 美化设置页面
-          },
-
-          // 本地化代理，支持多语言
-          localizationsDelegates: const [
-            S.delegate,
-            GlobalMaterialLocalizations.delegate,
-            GlobalCupertinoLocalizations.delegate,
-            GlobalWidgetsLocalizations.delegate
-          ],
-          
-          // 支持的语言列表
-          supportedLocales: S.delegate.supportedLocales,
-
-          // 语言回调，用于处理特定语言和地区的逻辑
-          localeResolutionCallback: (locale, supportedLocales) {
-            if (locale != null) {
-              // 处理中文的特殊区域
-              if (locale.languageCode == 'zh' && 
-                  (locale.countryCode == 'TW' || locale.countryCode == 'HK' || locale.countryCode == 'MO')) {
-                return const Locale('zh', 'TW');  // 繁体中文
-              }
-              // 处理简体中文
-              if (locale.languageCode == 'zh' && (locale.countryCode == 'CN' || locale.countryCode == null)) {
-                return const Locale('zh', 'CN');  // 简体中文
-              }
-              // 匹配合适的语言和国家代码
-              return supportedLocales.firstWhere(
-                (supportedLocale) => 
-                  supportedLocale.languageCode == locale.languageCode && 
-                  (supportedLocale.countryCode == locale.countryCode || supportedLocale.countryCode == null),
-                orElse: () => supportedLocales.first,
-              );
-            }
-            return supportedLocales.first;  // 默认使用第一个支持的语言
-          },
-
-          // 隐藏调试标志
-          debugShowCheckedModeBanner: false,
-
-          // 使用 SplashScreen 作为启动页
-          home: SplashScreen(),
-
-          // 全局构建器，处理悬浮日志按钮、文本缩放和加载动画
-          builder: (context, child) {
-            return Stack(
-              children: [
-                MediaQuery(
-                  data: MediaQuery.of(context).copyWith(textScaler: TextScaler.linear(data.textScaleFactor)),
-                  child: FlutterEasyLoading(child: child),  // 加载动画封装
+        return LogUtil.safeExecute(() {
+          // 返回 MaterialApp，配置应用的主题、语言、路由等
+          return MaterialApp(
+            title: 'ITVAPP LIVETV',  // 应用标题
+            
+            // 设置应用的主题，包括亮度、颜色方案、字体和其他 UI 样式
+            theme: ThemeData(
+                brightness: Brightness.dark,  // 使用暗色主题
+                fontFamily: fontFamily,  // 设置字体
+                colorScheme: ColorScheme.fromSeed(seedColor: Colors.redAccent, brightness: Brightness.dark),  // 基于种子颜色生成色系
+                scaffoldBackgroundColor: Colors.black,  // 背景色设为黑色
+                appBarTheme: const AppBarTheme(
+                  backgroundColor: Colors.black,  // AppBar 背景色
+                  foregroundColor: Colors.white,  // AppBar 文字颜色
+                  elevation: 0,  // 移除阴影
+                  centerTitle: true,  // 标题居中
                 ),
-                // 添加按钮，仅当 _debugMode 为 true 时显示
-                Visibility(
-                  visible: LogUtil._debugMode,  // 当 _debugMode 为 true 时显示按钮
-                  child: Positioned(
-                    bottom: 20,
-                    right: 20,
-                    child: Builder(  // 使用 Builder 生成新的上下文
-                      builder: (BuildContext newContext) {
-                        return FloatingActionButton(
-                          onPressed: () {
-                            Navigator.push(
-                              newContext,  // 使用新生成的上下文
-                              MaterialPageRoute(builder: (context) => LogViewerPage()),  // 跳转到日志查看页面
-                            );
-                          },
-                          child: Icon(Icons.view_list),
-                          tooltip: '查看日志',
-                        );
-                      },
+                useMaterial3: true),  // 使用 Material Design 3
+
+            // 设置应用的语言
+            locale: languageProvider.currentLocale,  
+
+            // 定义路由配置
+            routes: {
+              RouterKeys.subScribe: (BuildContext context) => const SubScribePage(),  // 订阅页面
+              RouterKeys.setting: (BuildContext context) => const SettingPage(),  // 设置页面
+              RouterKeys.settingFont: (BuildContext context) => const SettingFontPage(),  // 字体设置页面
+              RouterKeys.settingBeautify: (BuildContext context) => const SettingBeautifyPage(),  // 美化设置页面
+            },
+
+            // 本地化代理，支持多语言
+            localizationsDelegates: const [
+              S.delegate,
+              GlobalMaterialLocalizations.delegate,
+              GlobalCupertinoLocalizations.delegate,
+              GlobalWidgetsLocalizations.delegate
+            ],
+            
+            // 支持的语言列表
+            supportedLocales: S.delegate.supportedLocales,
+
+            // 语言回调，用于处理特定语言和地区的逻辑
+            localeResolutionCallback: (locale, supportedLocales) {
+              return LogUtil.safeExecute(() {
+                if (locale != null) {
+                  // 处理中文的特殊区域
+                  if (locale.languageCode == 'zh' && 
+                      (locale.countryCode == 'TW' || locale.countryCode == 'HK' || locale.countryCode == 'MO')) {
+                    return const Locale('zh', 'TW');  // 繁体中文
+                  }
+                  // 处理简体中文
+                  if (locale.languageCode == 'zh' && (locale.countryCode == 'CN' || locale.countryCode == null)) {
+                    return const Locale('zh', 'CN');  // 简体中文
+                  }
+                  // 匹配合适的语言和国家代码
+                  return supportedLocales.firstWhere(
+                    (supportedLocale) => 
+                      supportedLocale.languageCode == locale.languageCode && 
+                      (supportedLocale.countryCode == locale.countryCode || supportedLocale.countryCode == null),
+                    orElse: () => supportedLocales.first,
+                  );
+                }
+                return supportedLocales.first;  // 默认使用第一个支持的语言
+              }, '语言解析时发生错误');
+            },
+
+            // 隐藏调试标志
+            debugShowCheckedModeBanner: false,
+
+            // 使用 SplashScreen 作为启动页
+            home: SplashScreen(),
+
+            // 全局构建器，处理悬浮日志按钮、文本缩放和加载动画
+            builder: (context, child) {
+              return Stack(
+                children: [
+                  MediaQuery(
+                    data: MediaQuery.of(context).copyWith(textScaler: TextScaler.linear(data.textScaleFactor)),
+                    child: FlutterEasyLoading(child: child),  // 加载动画封装
+                  ),
+                  // 添加按钮，仅当 _debugMode 为 true 时显示
+                  Visibility(
+                    visible: LogUtil._debugMode,  // 当 _debugMode 为 true 时显示按钮
+                    child: Positioned(
+                      bottom: 20,
+                      right: 20,
+                      child: Builder(  // 使用 Builder 生成新的上下文
+                        builder: (BuildContext newContext) {
+                          return FloatingActionButton(
+                            onPressed: () {
+                              LogUtil.safeExecute(() {
+                                Navigator.push(
+                                  newContext,  // 使用新生成的上下文
+                                  MaterialPageRoute(builder: (context) => LogViewerPage()),  // 跳转到日志查看页面
+                                );
+                              }, '跳转到日志查看页面时发生错误');
+                            },
+                            child: Icon(Icons.view_list),
+                            tooltip: '查看日志',
+                          );
+                        },
+                      ),
                     ),
                   ),
-                ),
-              ],
-            );
-          },
-        );
+                ],
+              );
+            },
+          );
+        }, '构建 MyApp 时发生错误');
       },
     );
   }
