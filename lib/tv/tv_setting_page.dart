31eda5a8003cd0a439977a799619e63fb32ef5d4
电视宝 www.itvapp.net <service@itvapp.net>
2024-09-12 05:27:07 +0800

    Update tv_setting_page.dart

--- lib/tv/tv_setting_page.dart
+++ lib/tv/tv_setting_page.dart
@@ -2,7 +2,6 @@ import 'package:itvapp_live_tv/setting/setting_font_page.dart';
 import 'package:itvapp_live_tv/setting/subscribe_page.dart';
 import 'package:flutter/material.dart';
 import '../setting/setting_beautify_page.dart';
-import '../util/log_util.dart';  // 引入日志工具类
 
 class TvSettingPage extends StatefulWidget {
   const TvSettingPage({super.key});
@@ -16,75 +15,60 @@ class _TvSettingPageState extends State<TvSettingPage> {
 
   @override
   Widget build(BuildContext context) {
-    return LogUtil.safeExecute<Widget>(() {
-      return Row(
-        children: [
-          SizedBox(
-            width: 300,
-            child: Scaffold(
-              appBar: AppBar(
-                title: const Text('设置'),
-              ),
-              body: ListView(
-                children: [
-                  ListTile(
-                    leading: const Icon(Icons.subscriptions),
-                    title: const Text('订阅源'),
-                    selected: _selectedIndex == 0,
-                    autofocus: true,
-                    onTap: () {
-                      LogUtil.safeExecute(() {
-                        setState(() {
-                          _selectedIndex = 0;
-                        });
-                        LogUtil.v('选择了订阅源设置');
-                      }, '选择订阅源设置时发生错误');
-                    },
-                  ),
-                  ListTile(
-                    leading: const Icon(Icons.font_download),
-                    title: const Text('字体设置'),
-                    selected: _selectedIndex == 1,
-                    onTap: () {
-                      LogUtil.safeExecute(() {
-                        setState(() {
-                          _selectedIndex = 1;
-                        });
-                        LogUtil.v('选择了字体设置');
-                      }, '选择字体设置时发生错误');
-                    },
-                  ),
-                  ListTile(
-                    leading: const Icon(Icons.brush),
-                    title: const Text('美化'),
-                    selected: _selectedIndex == 2,
-                    onTap: () {
-                      LogUtil.safeExecute(() {
-                        setState(() {
-                          _selectedIndex = 2;
-                        });
-                        LogUtil.v('选择了美化设置');
-                      }, '选择美化设置时发生错误');
-                    },
-                  ),
-                ],
-              ),
+    return Row(
+      children: [
+        SizedBox(
+          width: 300,
+          child: Scaffold(
+            appBar: AppBar(
+              title: const Text('设置'),
+            ),
+            body: ListView(
+              children: [
+                ListTile(
+                  leading: const Icon(Icons.subscriptions),
+                  title: const Text('订阅源'),
+                  selected: _selectedIndex == 0,
+                  autofocus: true,
+                  onTap: () {
+                    setState(() {
+                      _selectedIndex = 0;
+                    });
+                  },
+                ),
+                ListTile(
+                  leading: const Icon(Icons.font_download),
+                  title: const Text('字体设置'),
+                  selected: _selectedIndex == 1,
+                  onTap: () {
+                    setState(() {
+                      _selectedIndex = 1;
+                    });
+                  },
+                ),
+                ListTile(
+                  leading: const Icon(Icons.brush),
+                  title: const Text('美化'),
+                  selected: _selectedIndex == 2,
+                  onTap: () {
+                    setState(() {
+                      _selectedIndex = 2;
+                    });
+                  },
+                ),
+              ],
             ),
           ),
-          if (_selectedIndex == 0)
-            const Expanded(
-              child: SubScribePage(),
-            ),
-          if (_selectedIndex == 1)
-            const Expanded(
-              child: SettingFontPage(),
-            ),
-          if (_selectedIndex == 2)
-            const Expanded(
-              child: SettingBeautifyPage(),
-            ),
-        ],
-      );
-    }, '构建设置页面时发生错误');
+        ),
+        if (_selectedIndex == 0)
+          const Expanded(
+            child: SubScribePage(), 
+          ),
+        if (_selectedIndex == 1)
+          const Expanded(child: SettingFontPage()), 
+        if (_selectedIndex == 2)
+          const Expanded(child: SettingBeautifyPage()), 
+      ],
+    );
   }
 }
