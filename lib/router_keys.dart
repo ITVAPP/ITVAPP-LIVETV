94e6b52f6582085450d949046b85b1c664e8698f
电视宝 www.itvapp.net <service@itvapp.net>
2024-09-12 18:36:14 +0800

    Update router_keys.dart

--- lib/router_keys.dart
+++ lib/router_keys.dart
@@ -1,7 +1,13 @@
 class RouterKeys {
+  // 私有化构造函数，防止实例化
   RouterKeys._();
+
+  // 路由常量
   static const String subScribe = '/subScribe';
   static const String setting = '/setting';
   static const String settingFont = '/setting/font';
   static const String settingBeautify = '/setting/beautify';
+
+  // 新增日志查看页面的路由
+  static const String logViewer = '/logViewer'; 
 }
