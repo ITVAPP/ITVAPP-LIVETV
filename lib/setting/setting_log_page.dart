02d4d1c07d9cd5dc3d1873b9c9b3992f25c2d5d9
电视宝 www.itvapp.net <service@itvapp.net>
2024-09-12 21:50:21 +0800

    Update setting_log_page.dart

--- lib/setting/setting_log_page.dart
+++ lib/setting/setting_log_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:itvapp_live_tv/util/log_util.dart'; // 导入日志工具类
+import 'package:itvapp_live_tv/provider/theme_provider.dart'; // 导入主题提供者
 
 /// 日志查看页面
 class SettinglogPage extends StatefulWidget {
@@ -11,6 +13,12 @@ class _SettinglogPageState extends State<SettinglogPage> {
   String _selectedLevel = 'all';
   int _logLimit = 100; // 初始加载条数
   bool _hasMoreLogs = true; // 是否还有更多日志
+
+  final _buttonShape = RoundedRectangleBorder(
+    borderRadius: BorderRadius.circular(30), // 统一圆角样式
+  );
+  final _selectedColor = const Color(0xFFEB144C); // 选中时颜色
+  final _unselectedColor = Colors.grey[300]; // 未选中时颜色
 
   // 获取有限的日志
   List<Map<String, String>> getLimitedLogs() {
@@ -44,79 +52,102 @@ class _SettinglogPageState extends State<SettinglogPage> {
   @override
   Widget build(BuildContext context) {
     List<Map<String, String>> logs = getLimitedLogs();
+    var screenWidth = MediaQuery.of(context).size.width;
+
+    bool isTV = context.watch<ThemeProvider>().isTV;
+    double maxContainerWidth = 580;
 
     return Scaffold(
+      backgroundColor: isTV ? const Color(0xFF1E2022) : null, // TV模式下背景颜色
       appBar: AppBar(
-        title: Text('日志查看器'),
+        leading: isTV ? const SizedBox.shrink() : null, // TV模式下不显示返回按钮
+        title: const Text('日志查看器'), // 页面标题
+        backgroundColor: isTV ? const Color(0xFF1E2022) : null, // TV模式下AppBar背景颜色
       ),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                _buildFilterButton('all', '所有日志'),
-                _buildFilterButton('v', 'Verbose'),
-                _buildFilterButton('e', 'Error'),
-                _buildFilterButton('i', 'Info'),
-                _buildFilterButton('d', 'Debug'),
-              ],
-            ),
+      body: Align(
+        alignment: Alignment.center, // 内容居中显示
+        child: Container(
+          constraints: BoxConstraints(
+            maxWidth: screenWidth > 580 ? maxContainerWidth : double.infinity, // 限制最大宽度
           ),
-          Expanded(
-            child: logs.isEmpty
-                ? Center(child: Text('暂无日志'))
-                : Scrollbar(
-                    thumbVisibility: true, // 替代 isAlwaysShown
-                    child: SingleChildScrollView(
-                      scrollDirection: Axis.horizontal,
-                      child: Column(
-                        children: [
-                          DataTable(
-                            columns: [
-                              DataColumn(label: Text('时间')),
-                              DataColumn(label: Text('类型')),
-                              DataColumn(label: Text('日志信息')),
-                            ],
-                            rows: logs
-                                .map((log) => DataRow(cells: [
-                                      DataCell(Text(formatDateTime(log['time']!))),
-                                      DataCell(Text(log['level']!)),
-                                      DataCell(Text(log['message']!)),
-                                    ]))
-                                .toList(),
-                          ),
-                          if (_hasMoreLogs) // 仅当有更多日志时显示加载更多按钮
-                            Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: ElevatedButton(
-                                onPressed: _loadMoreLogs,
-                                child: Text('加载更多'),
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(15),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    _buildFilterButton('all', '所有日志'),
+                    _buildFilterButton('v', 'Verbose'),
+                    _buildFilterButton('e', 'Error'),
+                    _buildFilterButton('i', 'Info'),
+                    _buildFilterButton('d', 'Debug'),
+                  ],
+                ),
+              ),
+              Expanded(
+                child: logs.isEmpty
+                    ? Center(child: Text('暂无日志'))
+                    : Scrollbar(
+                        thumbVisibility: true, // 替代 isAlwaysShown
+                        child: SingleChildScrollView(
+                          scrollDirection: Axis.horizontal,
+                          child: Column(
+                            children: [
+                              DataTable(
+                                columns: [
+                                  DataColumn(label: Text('时间')),
+                                  DataColumn(label: Text('类型')),
+                                  DataColumn(label: Text('日志信息')),
+                                ],
+                                rows: logs
+                                    .map((log) => DataRow(cells: [
+                                          DataCell(Text(formatDateTime(log['time']!))),
+                                          DataCell(Text(log['level']!)),
+                                          DataCell(Text(log['message']!)),
+                                        ]))
+                                    .toList(),
                               ),
-                            ),
-                        ],
+                              if (_hasMoreLogs) // 仅当有更多日志时显示加载更多按钮
+                                Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: ElevatedButton(
+                                    onPressed: _loadMoreLogs,
+                                    child: Text('加载更多'),
+                                    style: ElevatedButton.styleFrom(
+                                      shape: _buttonShape, // 统一圆角样式
+                                      backgroundColor: _selectedColor,
+                                    ),
+                                  ),
+                                ),
+                            ],
+                          ),
+                        ),
                       ),
-                    ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      LogUtil.clearLogs();
+                    });
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text('日志已清空'),
+                      ),
+                    );
+                  },
+                  child: Text('清空日志'),
+                  style: ElevatedButton.styleFrom(
+                    shape: _buttonShape, // 统一圆角样式
+                    backgroundColor: _selectedColor,
                   ),
+                ),
+              ),
+            ],
           ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: ElevatedButton(
-              onPressed: () {
-                setState(() {
-                  LogUtil.clearLogs();
-                });
-                ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(
-                    content: Text('日志已清空'),
-                  ),
-                );
-              },
-              child: Text('清空日志'),
-            ),
-          ),
-        ],
+        ),
       ),
     );
   }
@@ -134,9 +165,10 @@ class _SettinglogPageState extends State<SettinglogPage> {
         },
         child: Text(label),
         style: ElevatedButton.styleFrom(
+          shape: _buttonShape, // 统一圆角样式
           backgroundColor: _selectedLevel == level
-              ? Theme.of(context).colorScheme.secondary
-              : Colors.grey,
+              ? _selectedColor
+              : _unselectedColor,
         ),
       ),
     );
