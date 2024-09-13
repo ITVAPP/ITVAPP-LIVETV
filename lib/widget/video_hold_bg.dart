d1798beb63f62b5c40e8ff5df42eaada4d56b7d9
电视宝 www.itvapp.net <service@itvapp.net>
2024-09-13 18:40:31 +0800

    Update video_hold_bg.dart

--- lib/widget/video_hold_bg.dart
+++ lib/widget/video_hold_bg.dart
@@ -22,17 +22,23 @@ class VideoHoldBg extends StatefulWidget {
   _VideoHoldBgState createState() => _VideoHoldBgState();
 }
 
-class _VideoHoldBgState extends State<VideoHoldBg> {
+class _VideoHoldBgState extends State<VideoHoldBg> with SingleTickerProviderStateMixin {
   List<String> bingImgUrls = []; // 存储 Bing 图片的 URL
-  String currentBgUrl = ''; // 当前显示的背景图 URL
+  String currentBgUrl = 'assets/images/video_bg.png'; // 当前显示的背景图 URL
   int imgIndex = 0; // 当前 Bing 图片的索引
   Timer? _timer; // 定时器
-  bool isLocalBg = true; // 默认使用本地背景图片
   bool isLoading = true; // 是否处于加载状态
+  bool isAudio = false; // 是否是音频
+  late AnimationController _animationController;
+  late Animation<double> _fadeAnimation;
 
   @override
   void initState() {
     super.initState();
+
+    // 初始化动画控制器和淡入淡出效果
+    _animationController = AnimationController(duration: const Duration(seconds: 1), vsync: this);
+    _fadeAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(_animationController);
 
     LogUtil.safeExecute(() {
       // 监听视频播放结束或缓冲状态
@@ -42,6 +48,14 @@ class _VideoHoldBgState extends State<VideoHoldBg> {
             _timer?.cancel(); // 停止定时器
             LogUtil.v('视频播放停止或缓冲结束，定时器取消');
           }
+
+          // 判断播放内容是否为音频，尺寸为 null 或 0 认为是音频
+          final size = widget.videoController.value.size;
+          if (size == null || size.width == 0 || size.height == 0) {
+            isAudio = true;
+          } else {
+            isAudio = false;
+          }
         });
       });
     }, '初始化监听器失败');
@@ -50,7 +64,8 @@ class _VideoHoldBgState extends State<VideoHoldBg> {
   @override
   void dispose() {
     _timer?.cancel(); // 界面销毁时取消定时器
-    LogUtil.v('界面销毁，定时器取消');
+    _animationController.dispose(); // 销毁动画控制器
+    LogUtil.v('界面销毁，定时器和动画控制器取消');
     super.dispose();
   }
 
@@ -68,6 +83,7 @@ class _VideoHoldBgState extends State<VideoHoldBg> {
           bingImgUrls = urls;
           currentBgUrl = bingImgUrls[0]; // 初始设置为第一张图片
           isLoading = false; // 图片加载完成，隐藏进度条
+          _startBingImageTimer(); // 开始 Bing 图片定时器
           LogUtil.v('Bing 图片加载成功，当前背景图为第 1 张');
         });
       }
@@ -86,16 +102,12 @@ class _VideoHoldBgState extends State<VideoHoldBg> {
     LogUtil.safeExecute(() {
       if (bingImgUrls.isNotEmpty) {
         _timer = Timer.periodic(Duration(seconds: 30), (timer) {
-          if (imgIndex < bingImgUrls.length - 1) {
-            setState(() {
-              imgIndex++;
-              currentBgUrl = bingImgUrls[imgIndex]; // 切换到下一张图片
-              LogUtil.v('切换到第 $imgIndex 张 Bing 图片');
-            });
-          } else {
-            timer.cancel(); // 如果图片切换完毕，停止定时器
-            LogUtil.v('Bing 图片已全部显示，定时器停止');
-          }
+          setState(() {
+            imgIndex = (imgIndex + 1) % bingImgUrls.length; // 循环切换 Bing 图片
+            currentBgUrl = bingImgUrls[imgIndex];
+            _animationController.forward(from: 0.0); // 触发淡入动画
+            LogUtil.v('切换到第 $imgIndex 张 Bing 图片');
+          });
         });
       }
     }, '启动 Bing 图片定时器失败');
@@ -109,72 +121,74 @@ class _VideoHoldBgState extends State<VideoHoldBg> {
     // 获取视频缓冲状态
     bool isBuffering = widget.videoController.value.isBuffering;
 
-    // 使用 Selector 来监听 ThemeProvider 中的 isBingBg 状态
+    // 如果播放的是音频，通过 Selector 来监听 ThemeProvider 中的 isBingBg 状态
     return Selector<ThemeProvider, bool>(
       selector: (_, provider) => provider.isBingBg,
       builder: (BuildContext context, bool isBingBg, Widget? child) {
         LogUtil.safeExecute(() {
-          // 如果 isBingBg 为 true，获取 Bing 背景图片并开始切换
-          if (isBingBg && bingImgUrls.isEmpty) {
-            _fetchBingImages();
-            _startBingImageTimer();
-            LogUtil.v('正在使用 Bing 背景图片');
-          } else {
-            // 使用本地背景图片
-            currentBgUrl = 'assets/images/video_bg.png';
-            LogUtil.v('正在使用本地背景图片');
+          // 播放的是音频时，判断是否使用 Bing 背景
+          if (isAudio) {
+            if (isBingBg && bingImgUrls.isEmpty) {
+              _fetchBingImages(); // 加载 Bing 图片
+              LogUtil.v('正在使用 Bing 背景图片');
+            } else if (!isBingBg) {
+              currentBgUrl = 'assets/images/video_bg.png'; // 使用本地背景图片
+              LogUtil.v('正在使用本地背景图片');
+            }
           }
         }, '选择背景图片失败');
 
-        // 在缓冲状态下保持显示当前的 Bing 图片或视频帧
-        return Container(
-          padding: const EdgeInsets.only(top: 30, bottom: 30),
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              fit: BoxFit.cover,
-              image: isBuffering
-                  ? (widget.videoController.value.isPlaying
-                      ? NetworkImage(currentBgUrl) // 缓冲时显示当前的 Bing 图片或视频帧
-                      : AssetImage('assets/images/video_bg.png') as ImageProvider) // 保持 Bing 图片或使用本地图片
-                  : (isBingBg
-                      ? NetworkImage(currentBgUrl) // 显示 Bing 图片
-                      : AssetImage('assets/images/video_bg.png') as ImageProvider), // 显示本地图片
-            ),
+        return _buildBackground(isLoading, isBuffering, progressBarWidth);
+      },
+    );
+  }
+
+  Widget _buildBackground(bool isLoading, bool isBuffering, double progressBarWidth) {
+    return FadeTransition(
+      opacity: _fadeAnimation, // 应用淡入淡出效果
+      child: Container(
+        padding: const EdgeInsets.only(top: 30, bottom: 30),
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            fit: BoxFit.cover,
+            image: isLoading
+                ? AssetImage('assets/images/video_bg.png') // 加载时始终显示本地背景图
+                : NetworkImage(currentBgUrl), // 正常显示当前背景图片（Bing 或本地）
           ),
-          child: Stack(
-            children: [
-              if (isLoading || isBuffering) // 如果正在加载或缓冲，显示进度条
-                Positioned(
-                  bottom: 12,
-                  left: 0,
-                  right: 0,
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      GradientProgressBar(
-                        width: progressBarWidth,
-                        height: 5,
-                        duration: const Duration(seconds: 3),
-                      ),
-                      const SizedBox(height: 8),
-                      FittedBox(
-                        child: Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                          child: Text(
-                            widget.toastString ?? S.current.loading,
-                            style: const TextStyle(color: Colors.white, fontSize: 16),
-                          ),
+        ),
+        child: Stack(
+          children: [
+            if (isLoading || isBuffering) // 如果正在加载或缓冲，显示进度条和提示文字
+              Positioned(
+                bottom: 12,
+                left: 0,
+                right: 0,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    GradientProgressBar(
+                      width: progressBarWidth,
+                      height: 5,
+                      duration: const Duration(seconds: 3),
+                    ),
+                    const SizedBox(height: 8),
+                    FittedBox(
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                        child: Text(
+                          widget.toastString ?? S.current.loading,
+                          style: const TextStyle(color: Colors.white, fontSize: 16),
                         ),
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
-            ],
-          ),
-        );
-      },
+              ),
+          ],
+        ),
+      ),
     );
   }
 }
