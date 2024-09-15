f83acbcd9c395c7537b8f37e5cf74b17d5cd40ef
电视宝 www.itvapp.net <service@itvapp.net>
2024-09-15 20:18:11 +0800

    Update setting_beautify_page.dart

--- lib/setting/setting_beautify_page.dart
+++ lib/setting/setting_beautify_page.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:itvapp_live_tv/provider/theme_provider.dart';
-import 'package:itvapp_live_tv/util/log_util.dart'; // 导入日志工具
+import 'package:itvapp_live_tv/util/log_util.dart';
+import '../generated/l10n.dart';
 
 class SettingBeautifyPage extends StatefulWidget {
   const SettingBeautifyPage({super.key});
@@ -33,11 +34,9 @@ class _SettingBeautifyPageState extends State<SettingBeautifyPage> {
     try {
       // 获取当前屏幕的宽度
       var screenWidth = MediaQuery.of(context).size.width;
-      LogUtil.d('屏幕宽度: $screenWidth');
 
       // 通过 Provider 获取 isTV 的状态
       bool isTV = context.watch<ThemeProvider>().isTV;
-      LogUtil.d('当前设备模式: ${isTV ? "TV" : "非TV"}');
 
       // 设置最大容器宽度为 580，适用于大屏幕设备
       double maxContainerWidth = 580;
@@ -45,7 +44,7 @@ class _SettingBeautifyPageState extends State<SettingBeautifyPage> {
       return Scaffold(
         backgroundColor: isTV ? const Color(0xFF1E2022) : null, // 在 TV 模式下设置背景颜色
         appBar: AppBar(
-          title: const Text('美化'), // AppBar 标题
+          S.of(context).backgroundImageTitle,  // AppBar 标题
           backgroundColor: isTV ? const Color(0xFF1E2022) : null, // TV 模式下设置 AppBar 颜色
           leading: isTV ? const SizedBox.shrink() : null, // 如果是 TV 模式，隐藏返回按钮
         ),
@@ -66,15 +65,14 @@ class _SettingBeautifyPageState extends State<SettingBeautifyPage> {
                       child: SwitchListTile(
                         focusNode: _bingFocusNode, // 将焦点节点绑定到该组件
                         title: const Text(
-                          '每日Bing',
+                          S.of(context).dailyBing,
                           style: TextStyle(fontWeight: FontWeight.bold),
                         ), // 设置标题加粗
-                        subtitle: const Text('未播放时的屏幕背景，每日更换图片'), // 提示信息
+                        subtitle: Text(S.of(context).backgroundImageDescription),  // 提示信息
                         value: context.watch<ThemeProvider>().isBingBg, // 读取 Bing 背景状态
                         onChanged: (value) {
                           LogUtil.safeExecute(() {
                             context.read<ThemeProvider>().setBingBg(value); // 设置 Bing 背景状态
-                            LogUtil.i('每日Bing背景设置为: ${value ? "启用" : "禁用"}');
                           }, '设置每日Bing背景时发生错误');
                         },
                         activeColor: Colors.white, // 启用时滑块颜色
@@ -94,7 +92,7 @@ class _SettingBeautifyPageState extends State<SettingBeautifyPage> {
       LogUtil.logError('构建 SettingBeautifyPage 时发生错误', e, stackTrace);
       return Scaffold(
         body: const Center(
-          child: Text('加载页面时出错'), // 错误页面提示
+          child: Text(S.of(context).errorLoadingPage), // 错误页面提示
         ),
       );
     }
