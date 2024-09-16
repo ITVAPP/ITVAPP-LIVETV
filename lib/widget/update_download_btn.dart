7c3cf79a08921b0baccc4aadf9dfc1b78bb19099
电视宝 www.itvapp.net <service@itvapp.net>
2024-09-16 17:47:57 +0800

    Update update_download_btn.dart

--- lib/widget/update_download_btn.dart
+++ lib/widget/update_download_btn.dart
@@ -1,12 +1,11 @@
 import 'dart:io';
-
 import 'package:itvapp_live_tv/util/env_util.dart';
 import 'package:itvapp_live_tv/provider/download_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import '../provider/theme_provider.dart'; 
+import '../provider/theme_provider.dart';  // 导入 ThemeProvider
 import '../generated/l10n.dart';
-import 'package:itvapp_live_tv/util/log_util.dart'; 
+import 'package:itvapp_live_tv/util/log_util.dart';  // 导入 LogUtil 用于日志记录
 
 class UpdateDownloadBtn extends StatefulWidget {
   final String apkUrl;
@@ -22,15 +21,16 @@ class _UpdateDownloadBtnState extends State<UpdateDownloadBtn> {
 
   @override
   Widget build(BuildContext context) {
-    // 通过 Provider 获取 isTV 的状态
-    bool isTV = context.watch<ThemeProvider>().isTV;
-    double btnWidth = isTV ? 400 : 260;
+    // 判断当前屏幕方向是否为横屏
+    bool isLandscape = MediaQuery.of(context).orientation == Orientation.landscape;
+    // 在横屏时使用TV端的按钮宽度
+    double btnWidth = isLandscape ? 400 : 260;
 
     return Consumer<DownloadProvider>(
       builder: (BuildContext context, DownloadProvider provider, Widget? child) {
         return provider.isDownloading
             ? ClipRRect(
-                borderRadius: BorderRadius.circular(48),
+                borderRadius: BorderRadius.circular(38),
                 child: SizedBox(
                   height: 48,
                   width: btnWidth,
@@ -45,9 +45,8 @@ class _UpdateDownloadBtnState extends State<UpdateDownloadBtn> {
                           color: Colors.redAccent,
                         ),
                       ),
-                      // 修改的部分：国际化“下载中”文本
                       Text(
-                        '${S.of(context).downloading}...${(provider.progress * 100).toStringAsFixed(1)}%', // 显示国际化的“下载中”和进度百分比
+                        '${S.of(context).downloading}...${(provider.progress * 100).toStringAsFixed(1)}%',
                         style: const TextStyle(color: Colors.white),
                       )
                     ],
@@ -56,40 +55,40 @@ class _UpdateDownloadBtnState extends State<UpdateDownloadBtn> {
               )
             : child!;
       },
-    );
-    child: ElevatedButton(
-      style: ElevatedButton.styleFrom(
-        fixedSize: Size(btnWidth, 48),
-        backgroundColor: _isFocusDownload ? Colors.redAccent : Colors.redAccent.withOpacity(0.3),
-        elevation: _isFocusDownload ? 10 : 0,
-        overlayColor: Colors.transparent,
-      ),
-      autofocus: true,
-      onFocusChange: (bool isFocus) {
-        setState(() {
-          _isFocusDownload = isFocus;
-        });
-      },
-      onPressed: () {
-        LogUtil.safeExecute(() {
-          if (Platform.isAndroid) {
-            try {
-              context.read<DownloadProvider>().downloadApk(widget.apkUrl);
-            } catch (e, stackTrace) {
-              LogUtil.logError('下载 APK 时发生错误', e, stackTrace);  // 异常日志记录
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          fixedSize: Size(btnWidth, 48),  // 横屏时的按钮宽度
+          backgroundColor: _isFocusDownload ? Colors.redAccent : Colors.redAccent.withOpacity(0.3),
+          elevation: _isFocusDownload ? 10 : 0,
+          overlayColor: Colors.transparent,
+        ),
+        autofocus: true,
+        onFocusChange: (bool isFocus) {
+          setState(() {
+            _isFocusDownload = isFocus;
+          });
+        },
+        onPressed: () {
+          LogUtil.safeExecute(() {
+            if (Platform.isAndroid) {
+              try {
+                context.read<DownloadProvider>().downloadApk(widget.apkUrl);
+              } catch (e, stackTrace) {
+                LogUtil.logError('下载 APK 时发生错误', e, stackTrace);  // 异常日志记录
+              }
+            } else {
+              try {
+                Navigator.of(context).pop(true);
+              } catch (e, stackTrace) {
+                LogUtil.logError('关闭对话框时发生错误', e, stackTrace);  // 异常日志记录
+              }
             }
-          } else {
-            try {
-              Navigator.of(context).pop(true);
-            } catch (e, stackTrace) {
-              LogUtil.logError('关闭对话框时发生错误', e, stackTrace);  // 异常日志记录
-            }
-          }
-        }, '点击下载按钮时发生错误');
-      },
-      child: Text(
-        S.current.update,
-        style: const TextStyle(color: Colors.white),
+          }, '点击下载按钮时发生错误');
+        },
+        child: Text(
+          S.current.update,
+          style: const TextStyle(color: Colors.white),
+        ),
       ),
     );
   }
