26838443f9c43a8929bf6dd15bb992525602b5fb
电视宝 www.itvapp.net <service@itvapp.net>
2024-09-23 02:45:15 +0800

    Update mobile_video_widget.dart

--- lib/mobile_video_widget.dart
+++ lib/mobile_video_widget.dart
@@ -92,7 +92,7 @@ class _MobileVideoWidgetState extends State<MobileVideoWidget> {
             icon: const Icon(Icons.add),  // 添加频道的图标
           ),
           // 以控制图标间距
-          const SizedBox(width: 8),
+          const SizedBox(width: 5),
           // 设置按钮
           IconButton(
             onPressed: () async {
