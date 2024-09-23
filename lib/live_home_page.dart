407c326b4ecbd555733e69579726700cd21a4205
电视宝 www.itvapp.net <service@itvapp.net>
2024-09-23 18:56:16 +0800

    Update live_home_page.dart

--- lib/live_home_page.dart
+++ lib/live_home_page.dart
@@ -43,25 +43,25 @@ class _LiveHomePageState extends State<LiveHomePage> {
   // 存储加载状态的提示文字
   String toastString = S.current.loading;
 
-  // 视频播放列表的数据模型，包含多个播放频道及其视频源
+  // 视频播放列表的数据模型
   PlaylistModel? _videoMap;
 
   // 当前播放的频道数据模型
   PlayModel? _currentChannel;
 
-  // 当前选中的视频源索引，表示正在播放哪个线路的地址
+  // 当前选中的视频源索引
   int _sourceIndex = 0;
 
-  // 视频播放器控制器，用于控制视频的播放、暂停等操作
+  // 视频播放器控制器
   VideoPlayerController? _playerController;
 
-  // 标记播放器是否处于缓冲状态
+  // 是否处于缓冲状态
   bool isBuffering = false;
 
-  // 标记播放器是否正在播放
+  // 是否正在播放
   bool isPlaying = false;
 
-  // 视频的宽高比，用于调整视频显示比例
+  // 视频的宽高比
   double aspectRatio = 1.78;
 
   // 标记侧边抽屉（频道选择）是否打开
@@ -70,19 +70,19 @@ class _LiveHomePageState extends State<LiveHomePage> {
   // 调试模式开关，调试时为 true，生产环境为 false
   bool isDebugMode = false;
 
-  // 重试次数计数器，记录当前播放重试的次数
+  // 重试次数计数器
   int _retryCount = 0;
 
   // 最大重试次数
   final int maxRetries = defaultMaxRetries;
 
-  // 标志是否等待超时检测
+  // 等待超时检测
   bool _timeoutActive = false;
 
-  // 标志播放器是否处于释放状态
+  // 是否处于释放状态
   bool _isDisposing = false;
 
-  // 防止快速切换时的竞态条件
+  // 快速切换时的竞态条件
   bool _isSwitchingChannel = false;
 
   // 超时检测时间
@@ -90,16 +90,18 @@ class _LiveHomePageState extends State<LiveHomePage> {
 
   StreamUrl? _streamUrl; 
 
-  /// 每次播放新视频前，解析当前频道的视频源，并进行播放。
-Future<void> _playVideo() async {
-  	
-LogUtil.i('触发播放前检查频道：$_currentChannel');
-LogUtil.i('触发播放前检查竞态条件：$_isSwitchingChannel');
-LogUtil.i('触发播放前检查资源释放：$_isDisposing');
+  // 收藏列表相关
+  PlaylistModel favoriteList = PlaylistModel(playList: {}); 
+
+  /// 播放新视频前，解析当前频道的视频源
+  Future<void> _playVideo() async {
+
+    LogUtil.i('播放前检查竞态条件：$_isSwitchingChannel');
+    LogUtil.i('播放前检查资源释放：$_isDisposing');
 
     if (_currentChannel == null || _isSwitchingChannel || _isDisposing) return;
 
-    // 在开始播放之前，释放旧的资源
+    // 释放旧的资源
     await _disposePlayer();
     
     _isSwitchingChannel = true;
@@ -111,13 +113,13 @@ LogUtil.i('触发播放前检查资源释放：$_isDisposing');
     // 获取当前视频源的 URL
     String url = _currentChannel!.urls![_sourceIndex].toString();
 
-    // 使用 StreamUrl 类解析并处理一些特定的视频源
+    // 解析特定的视频源
     _streamUrl = StreamUrl(url);
     try {
       // 获取解析后的有效视频 URL
       String parsedUrl = await _streamUrl!.getStreamUrl();
 
-      // 如果解析失败，返回 'ERROR'，则显示错误信息并终止播放
+      // 如果解析失败，返回 'ERROR'
       if (parsedUrl == 'ERROR') {
         setState(() {
           toastString = S.current.playError;
@@ -144,7 +146,7 @@ LogUtil.i('触发播放前检查资源释放：$_isDisposing');
     }
 
     try {
-      // 创建视频播放器控制器并初始化，使用解析后的 URL 播放视频
+      // 创建视频播放器控制器并初始化
       _playerController = VideoPlayerController.networkUrl(
         Uri.parse(url),
         videoPlayerOptions: VideoPlayerOptions(
@@ -156,12 +158,12 @@ LogUtil.i('触发播放前检查资源释放：$_isDisposing');
       await _playerController?.initialize();
       _playerController?.play();
       setState(() {
-        toastString = S.current.loading; // 更新 UI，显示加载状态
+        toastString = S.current.loading; // 显示加载状态
       });
 
       // 播放成功，重置重试次数计数器
       _retryCount = 0;
-      _timeoutActive = false; // 播放成功，取消超时检测
+      _timeoutActive = false;
       _playerController?.addListener(_videoListener); 
 
       // 添加超时检测机制
@@ -176,9 +178,7 @@ LogUtil.i('触发播放前检查资源释放：$_isDisposing');
 
   /// 播放器资源释放
   Future<void> _disposePlayer() async {
-    // 释放旧的 StreamUrl 实例
     _disposeStreamUrl();
-
     if (!_isDisposing) {
       _isDisposing = true;
       try {
@@ -246,22 +246,22 @@ LogUtil.i('触发播放前检查资源释放：$_isDisposing');
   }
 
   /// 显示播放确认对话框
-Future<bool> _showConfirmationDialog(BuildContext context, String url) async {
-  return await DialogUtil.showCustomDialog(
-    context,
-    title: S.of(context).foundStreamTitle,  // 动态传入标题
-    content: S.of(context).streamUrlContent(url),  // 动态传入内容
-    positiveButtonLabel: S.of(context).playButton,  // 正向按钮文本：播放
-    onPositivePressed: () {
-      Navigator.of(context).pop(true);  // 用户确认播放
-    },
-    negativeButtonLabel: S.of(context).cancelButton,  // 负向按钮文本：取消
-    onNegativePressed: () {
-      Navigator.of(context).pop(false);  // 用户取消播放
-    },
-    isDismissible: false,  // 禁止点击对话框外部关闭
-  ) ?? false;  // 如果对话框意外关闭，返回 false
-}
+  Future<bool> _showConfirmationDialog(BuildContext context, String url) async {
+    return await DialogUtil.showCustomDialog(
+      context,
+      title: S.of(context).foundStreamTitle,  // 动态传入标题
+      content: S.of(context).streamUrlContent(url),  // 动态传入内容
+      positiveButtonLabel: S.of(context).playButton, 
+      onPositivePressed: () {
+        Navigator.of(context).pop(true);  // 用户确认播放
+      },
+      negativeButtonLabel: S.of(context).cancelButton, 
+      onNegativePressed: () {
+        Navigator.of(context).pop(false);  // 用户取消播放
+      },
+      isDismissible: false,  // 禁止点击外部关闭
+    ) ?? false; 
+  }
 
   /// 监听视频播放状态的变化
   void _videoListener() {
@@ -269,7 +269,7 @@ Future<bool> _showConfirmationDialog(BuildContext context, String url) async {
 
     // 如果发生播放错误，则切换到下一个视频源
     if (_playerController!.value.hasError) {
-      _retryPlayback(); // 调用失败处理逻辑
+      _retryPlayback();
       return;
     }
 
@@ -310,48 +310,147 @@ Future<bool> _showConfirmationDialog(BuildContext context, String url) async {
 
     // 加载播放列表数据和版本检测
     _loadData();
+    
+    // 加载收藏列表
+    _loadFavorites();
+  }
+
+  /// 加载或初始化“我的收藏”列表
+  Future<void> _loadFavorites() async {
+    final favoriteData = await _getCachedFavoriteM3uData();
+  
+    if (favoriteData.isEmpty) {
+      // 如果没有缓存数据，初始化空的收藏列表
+      favoriteList = PlaylistModel(
+        playList: {"我的收藏": <String, Map<String, PlayModel>>{}},
+      );
+    } else {
+      // 如果有缓存数据，解析并加载“我的收藏”列表
+      favoriteList = PlaylistModel.fromString(favoriteData);
+    }
+    setState(() {});
+  }
+
+  /// 从本地缓存中获取“我的收藏”列表
+  static Future<String> _getCachedFavoriteM3uData() async {
+    try {
+      return SpUtil.getString(favoriteCacheKey, defValue: '') ?? '';
+    } catch (e, stackTrace) {
+      LogUtil.logError('获取本地缓存的“我的收藏”列表失败', e, stackTrace);
+      return '';
+    }
+  }
+
+  /// 保存更新后的“我的收藏”列表到本地缓存
+  static Future<void> _saveFavoriteList(PlaylistModel favoritePlaylist) async {
+    await SpUtil.putString(favoriteCacheKey, favoritePlaylist.toString());
+  }
+
+  // 获取当前频道的分组名字
+  String getGroupName(String channelId) {
+    return _currentChannel?.group ?? '';
+  }
+
+  // 获取当前频道的名字
+  String getChannelName(String channelId) {
+    return _currentChannel?.title ?? '';
+  }
+
+  // 获取当前频道的播放地址列表
+  List<String> getPlayUrls(String channelId) {
+    return _currentChannel?.urls ?? [];
+  }
+
+  // 检查当前频道是否已收藏
+  bool isChannelFavorite(String channelId) {
+    return favoriteList.playList['我的收藏']?.containsKey(channelId) ?? false;
+  }
+
+  // 添加或取消收藏
+  void toggleFavorite(String channelId) {
+    // 确保 '我的收藏' 结构已存在
+    if (favoriteList.playList['我的收藏'] == null) {
+      favoriteList.playList['我的收藏'] = {};
+    }
+
+    bool isFavoriteChanged = false;
+
+    if (isChannelFavorite(channelId)) {
+      // 取消收藏
+      favoriteList.playList['我的收藏']!.remove(channelId);
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('频道已从收藏中移除'), duration: Duration(seconds: 2))
+      );
+      isFavoriteChanged = true;
+    } else {
+      // 添加收藏
+      PlayModel newFavorite = PlayModel(
+        id: channelId,
+        title: getChannelName(channelId),
+        group: getGroupName(channelId),
+        urls: getPlayUrls(channelId),
+        logo: _currentChannel?.logo,
+      );
+      favoriteList.playList['我的收藏']![channelId] = newFavorite;
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('频道已添加到收藏'), duration: Duration(seconds: 2))
+      );
+      isFavoriteChanged = true;
+    }
+
+    // 仅在收藏状态改变时更新UI
+    if (isFavoriteChanged) {
+      _saveFavoriteList(favoriteList).then((success) {
+        if (success) {
+          setState(() {});  // 更新UI状态
+        } else {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text('保存收藏状态失败'), duration: Duration(seconds: 2))
+          );
+        }
+      });
+    }
   }
 
   /// 从播放列表中动态提取频道，处理两层和三层结构
-PlayModel? _getChannelFromPlaylist(Map<String, dynamic> playList) {
-  // 遍历每个分类
-  for (String category in playList.keys) {
-    if (playList[category] is Map<String, Map<String, PlayModel>>) {
-      // 三层结构处理
-      Map<String, Map<String, PlayModel>> groupMap = playList[category];
+  PlayModel? _getChannelFromPlaylist(Map<String, dynamic> playList) {
+    // 遍历每个分类
+    for (String category in playList.keys) {
+      if (playList[category] is Map<String, Map<String, PlayModel>>) {
+        // 三层结构处理
+        Map<String, Map<String, PlayModel>> groupMap = playList[category];
 
-      // 遍历每个组
-      for (String group in groupMap.keys) {
-        Map<String, PlayModel> channelMap = groupMap[group] ?? {};
+        for (String group in groupMap.keys) {
+          Map<String, PlayModel> channelMap = groupMap[group] ?? {};
 
-        // 遍历每个频道，返回第一个有有效播放地址的频道
+          // 遍历频道返回第一个有效播放地址
+          for (PlayModel? channel in channelMap.values) {
+            if (channel?.urls != null && channel!.urls!.isNotEmpty) {
+              return channel;  
+            }
+          }
+        }
+      } else if (playList[category] is Map<String, PlayModel>) {
+        // 两层结构处理
+        Map<String, PlayModel> channelMap = playList[category] ?? {};
+
+        // 遍历频道返回第一个有效播放地址
         for (PlayModel? channel in channelMap.values) {
           if (channel?.urls != null && channel!.urls!.isNotEmpty) {
-            return channel;  // 找到第一个可用的频道，立即返回
+            return channel; 
           }
         }
       }
-    } else if (playList[category] is Map<String, PlayModel>) {
-      // 两层结构处理
-      Map<String, PlayModel> channelMap = playList[category] ?? {};
-
-      // 遍历每个频道，返回第一个有有效播放地址的频道
-      for (PlayModel? channel in channelMap.values) {
-        if (channel?.urls != null && channel!.urls!.isNotEmpty) {
-          return channel;  // 找到第一个可用的频道，立即返回
-        }
-      }
     }
+    return null;  // 如果所有频道都没有找到可用的
   }
-  return null;  // 如果遍历所有频道都没有找到可用的，返回 null
-}
 
   /// 异步加载视频数据和版本检测
   _loadData() async {
     try {
       _videoMap = widget.m3uData;
       _sourceIndex = 0;
-      await _handlePlaylist(); // 处理播放列表和 EPG 数据
+      await _handlePlaylist();
       CheckVersionUtil.checkVersion(context, false, false);
     } catch (e, stackTrace) {
       LogUtil.logError('加载数据时出错', e, stackTrace);
@@ -365,51 +464,49 @@ PlayModel? _getChannelFromPlaylist(Map<String, dynamic> playList) {
       final resMap = await M3uUtil.getLocalM3uData(); // 获取播放列表数据
       _videoMap = resMap.data;
       _sourceIndex = 0;
-
-      await _handlePlaylist(); // 处理播放列表和 EPG 数据
+      await _handlePlaylist();
     } catch (e, stackTrace) {
       LogUtil.logError('解析播放列表时出错', e, stackTrace);
     }
   }
 
   /// 处理播放列表和 EPG 数据的通用逻辑
-Future<void> _handlePlaylist() async {
-  if (_videoMap?.playList?.isNotEmpty ?? false) {
-    // 获取第一个可用的频道，如果该分类为空，跳过它
-    _currentChannel = _getChannelFromPlaylist(_videoMap!.playList!);
+  Future<void> _handlePlaylist() async {
+    if (_videoMap?.playList?.isNotEmpty ?? false) {
+      // 获取第一个可用的频道，如果该分类为空，跳过它
+      _currentChannel = _getChannelFromPlaylist(_videoMap!.playList!);
 
-    if (_currentChannel != null) {
-      // 确保不会重复调用播放方法
-      if (!_isSwitchingChannel && !_isDisposing) {
+      if (_currentChannel != null) {
+        if (!_isSwitchingChannel && !_isDisposing) {
+          setState(() {
+            _playVideo(); // 播放第一个可用的频道
+          });
+        }
+      } else {
+        // 没有可用的频道
         setState(() {
-          _playVideo(); // 播放第一个可用的频道
+          toastString = 'UNKNOWN';
         });
       }
+
+      // 处理 EPG 数据加载
+      if (_videoMap?.epgUrl?.isNotEmpty ?? false) {
+        try {
+          EpgUtil.loadEPGXML(_videoMap!.epgUrl!); 
+        } catch (e, stackTrace) {
+          LogUtil.logError('加载EPG数据时出错', e, stackTrace); 
+        }
+      } else {
+        EpgUtil.resetEPGXML(); // 重置EPG数据
+      }
     } else {
-      // 没有可用的频道
+      // 播放列表为空
       setState(() {
+        _currentChannel = null; 
         toastString = 'UNKNOWN';
       });
     }
-
-    // 处理 EPG 数据加载
-    if (_videoMap?.epgUrl?.isNotEmpty ?? false) {
-      try {
-        EpgUtil.loadEPGXML(_videoMap!.epgUrl!); 
-      } catch (e, stackTrace) {
-        LogUtil.logError('加载EPG数据时出错', e, stackTrace); 
-      }
-    } else {
-      EpgUtil.resetEPGXML(); // 重置EPG数据
-    }
-  } else {
-    // 播放列表为空
-    setState(() {
-      _currentChannel = null; 
-      toastString = 'UNKNOWN';
-    });
   }
-}
 
   @override
   void dispose() {
@@ -516,77 +613,76 @@ Future<void> _handlePlaylist() async {
   }
 
   /// 通过底部弹出框选择不同的视频源
-Future<void> _changeChannelSources() async {
-  List<String>? sources = _currentChannel?.urls; 
-  // 如果 sources 为空或不存在，记录日志
-  if (sources == null || sources.isEmpty) {
-    LogUtil.e('未找到有效的视频源');
-    return;
-  }
+  Future<void> _changeChannelSources() async {
+    List<String>? sources = _currentChannel?.urls; 
+    if (sources == null || sources.isEmpty) {
+      LogUtil.e('未找到有效的视频源');
+      return;
+    }
 
-  try {
-    // 显示选择线路的弹窗
-    final selectedIndex = await showModalBottomSheet(
-      context: context,
-      useRootNavigator: true,
-      barrierColor: Colors.transparent,
-      backgroundColor: Colors.black45,
-      builder: (BuildContext context) {
-        return SingleChildScrollView(
-          child: Container(
-            width: double.infinity,
-            padding: const EdgeInsets.only(left: 20, right: 20, top: 20, bottom: 20),
-            color: Colors.transparent,
-            child: ConstrainedBox(
-              constraints: BoxConstraints(
-                maxWidth: MediaQuery.of(context).size.width * 0.7,
-              ),
-              child: Wrap(
-                spacing: 10,   
-                runSpacing: 10, 
-                children: List.generate(sources.length, (index) {
-                  return ConstrainedBox(
-                    constraints: BoxConstraints(minWidth: 60), 
-                    child: OutlinedButton(
-                      autofocus: _sourceIndex == index,
-                      style: OutlinedButton.styleFrom(
-                        padding: EdgeInsets.symmetric(vertical: 2, horizontal: 6), 
-                        backgroundColor: _sourceIndex == index ? Color(0xFFEB144C) : Colors.grey[300]!, 
-                        side: BorderSide(color: _sourceIndex == index ? Color(0xFFEB144C) : Colors.grey[300]!),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(12), 
+    try {
+      // 显示选择线路的弹窗
+      final selectedIndex = await showModalBottomSheet(
+        context: context,
+        useRootNavigator: true,
+        barrierColor: Colors.transparent,
+        backgroundColor: Colors.black45,
+        builder: (BuildContext context) {
+          return SingleChildScrollView(
+            child: Container(
+              width: double.infinity,
+              padding: const EdgeInsets.only(left: 20, right: 20, top: 20, bottom: 20),
+              color: Colors.transparent,
+              child: ConstrainedBox(
+                constraints: BoxConstraints(
+                  maxWidth: MediaQuery.of(context).size.width * 0.7,
+                ),
+                child: Wrap(
+                  spacing: 10,   
+                  runSpacing: 10, 
+                  children: List.generate(sources.length, (index) {
+                    return ConstrainedBox(
+                      constraints: BoxConstraints(minWidth: 60), 
+                      child: OutlinedButton(
+                        autofocus: _sourceIndex == index,
+                        style: OutlinedButton.styleFrom(
+                          padding: EdgeInsets.symmetric(vertical: 2, horizontal: 6), 
+                          backgroundColor: _sourceIndex == index ? Color(0xFFEB144C) : Colors.grey[300]!, 
+                          side: BorderSide(color: _sourceIndex == index ? Color(0xFFEB144C) : Colors.grey[300]!),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12), 
+                          ),
+                        ),
+                        onPressed: _sourceIndex == index
+                            ? null
+                            : () {
+                                Navigator.pop(context, index);
+                              },
+                        child: Text(
+                          S.current.lineIndex(index + 1),
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 15,
+                            color: _sourceIndex == index ? Colors.white : Colors.black, 
+                          ),
                         ),
                       ),
-                      onPressed: _sourceIndex == index
-                          ? null
-                          : () {
-                              Navigator.pop(context, index);
-                            },
-                      child: Text(
-                        S.current.lineIndex(index + 1),
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          fontSize: 14,
-                          color: _sourceIndex == index ? Colors.white : Colors.black, 
-                        ),
-                      ),
-                    ),
-                  );
-                }),
+                    );
+                  }),
+                ),
               ),
             ),
-          ),
-        );
-      },
-    );
+          );
+        },
+      );
 
-    // 切换到选中的视频源并播放
-    if (selectedIndex != null && _sourceIndex != selectedIndex) {
-      _sourceIndex = selectedIndex;
-      _playVideo();
-    } 
-  } catch (modalError, modalStackTrace) {
-    LogUtil.logError('弹出窗口时出错', modalError, modalStackTrace);
+      // 切换到选中的视频源并播放
+      if (selectedIndex != null && _sourceIndex != selectedIndex) {
+        _sourceIndex = selectedIndex;
+        _playVideo();
+      } 
+    } catch (modalError, modalStackTrace) {
+      LogUtil.logError('弹出窗口时出错', modalError, modalStackTrace);
+    }
   }
-}
 }
