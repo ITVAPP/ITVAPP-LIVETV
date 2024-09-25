e089115c3b944d3845a7867c901cbaf41be6ff48
电视宝 www.itvapp.net <service@itvapp.net>
2024-09-25 22:16:48 +0800

    Create config.dart

---
+++ lib/config.dart
@@ -0,0 +1,13 @@
+class Config {
+  /// 定义收藏列表的本地缓存键
+  static const String favoriteCacheKey = 'favorite_m3u_cache';
+
+  /// 定义播放列表的本地缓存键
+  static const String m3uCacheKey = 'm3u_cache';
+
+  /// 定义收藏列表的分类名称
+  static const String myFavoriteKey = '我的收藏';
+
+  /// 定义播放列表无分类时的分类名称
+  static const String allChannelsKey = '所有频道';
+}
