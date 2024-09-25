57aebdea1e043140f4a8f1b50f9214b347ad4e71
电视宝 www.itvapp.net <service@itvapp.net>
2024-09-25 16:58:16 +0800

    Update log_util.dart

--- lib/util/log_util.dart
+++ lib/util/log_util.dart
@@ -41,8 +41,7 @@ class LogUtil {
       return;
     }
 
-    final timestamp = DateTime.now().toIso8601String();
-    _log('e', '[$timestamp] 错误: $message\n错误详情: $error\n堆栈信息: ${_processStackTrace(stackTrace)}', _defTag);
+    _log('e', '错误: $message\n错误详情: $error\n堆栈信息: ${_processStackTrace(stackTrace)}', _defTag);
   }
 
   // 安全执行方法，捕获并记录异常
@@ -80,7 +79,6 @@ class LogUtil {
   static void _log(String level, Object? object, String? tag) {
     if (!debugMode || object == null) return;
 
-    String time = DateTime.now().toString();
     String fileInfo = _getFileAndLine(); // 获取文件和行号信息
     // 安全处理 object，避免出现 null 值导致错误
     String logMessage = '${tag ?? _defTag} $level | ${object?.toString() ?? 'null'} - $fileInfo';
@@ -90,7 +88,7 @@ class LogUtil {
       _logs.removeAt(0); // 移除最旧的一条日志
     }
 
-    _logs.add({'time': time, 'level': level, 'message': logMessage});
+    _logs.add({'level': level, 'message': logMessage});
     developer.log(logMessage);
   }
 
