ea6815ebb2738d3b47c4cc588000a10c173327f2
电视宝 www.itvapp.net <service@itvapp.net>
2024-09-26 17:07:32 +0800

    Update live_home_page.dart

--- lib/live_home_page.dart
+++ lib/live_home_page.dart
@@ -414,12 +414,8 @@ class _LiveHomePageState extends State<LiveHomePage> {
       LogUtil.i('更新收藏检查收藏列表: ${favoriteItem}');
 
         // 更新播放列表中的收藏部分
-        if (favoriteItem is Map<String, Map<String, PlayModel>>) {
-          // 如果 favoriteItem 是 Map<String, Map<String, PlayModel>> 类型
-          _videoMap?.playList[Config.myFavoriteKey] = favoriteItem;
-        } else  {
-          _videoMap?.playList[Config.myFavoriteKey] = favoriteItem.cast<String, Map<String, PlayModel>>();
-        }
+      _videoMap?.playList[Config.myFavoriteKey] = favoriteList.playList[Config.myFavoriteKey];
+        
       LogUtil.i('修改收藏后的播放列表: ${_videoMap}');
         // 保存更新后的播放列表到缓存
         await M3uUtil.saveCachedM3uData(_videoMap.toString());
