1695c9287ecfc74d201ad0ebea3cafd9127ff6df
电视宝 www.itvapp.net <service@itvapp.net>
2024-10-03 03:31:52 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -30,28 +30,27 @@ void main() async {
   // 确保 WidgetsFlutterBinding 已经初始化，必要时会为应用的生命周期提供必要的绑定。
   WidgetsFlutterBinding.ensureInitialized();
 
-  
   // 启用 WakelockPlus 以防止屏幕锁定（用于移动设备）
   WakelockPlus.enable();
-  
+
   // 初始化共享存储的工具实例，用于管理存储操作
   await SpUtil.getInstance();
 
   // 初始化 ThemeProvider 并确保它的初始化完成
   ThemeProvider themeProvider = ThemeProvider();
   await themeProvider.initialize();  // 等待 ThemeProvider 完全初始化
-  
+
   // 注册 FVP 播放器，支持不同平台和解码器
   fvp.registerWith(options: {
     'platforms': ['android', 'ios'],  // 支持的平台
     'video.decoders': ['FFmpeg']  // 使用 FFmpeg 进行视频解码
   });
-  
+
   // 如果当前环境不是移动端
   if (!EnvUtil.isMobile) {
     // 初始化窗口管理器（用于桌面端窗口管理）
     await windowManager.ensureInitialized();
-    
+
     // 设置窗口的选项，如窗口大小、最小大小、是否居中、背景透明等
     WindowOptions windowOptions = const WindowOptions(
       size: Size(414, 414 * 16 / 9),  // 窗口初始大小
@@ -62,7 +61,7 @@ void main() async {
       titleBarStyle: TitleBarStyle.hidden,  // 隐藏标题栏
       title: 'ITVAPP LIVETV',  // 窗口标题
     );
-    
+
     // 等待窗口准备好后展示并聚焦
     windowManager.waitUntilReadyToShow(windowOptions, () async {
       await windowManager.show();  // 显示窗口
@@ -100,20 +99,56 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  static const int screenCheckDelay = 500;  // 延迟时间设置
+
   @override
   void initState() {
     super.initState();
     // 只在启动时检测 TV 状态并设置
     // themeProvider.checkAndSetIsTV(); // 只检测一次
-    
+
     // 初始化日志开关，但不自动触发 UI 更新
     // LogUtil.updateDebugModeFromProvider(context);
+  }
+
+  // 处理返回键的逻辑
+  Future<bool> _handleBackPress(BuildContext context) async {
+    // 检查当前页面是否是 SplashScreen 或者即将返回的上一页是 SplashScreen
+    bool isSplashScreen = ModalRoute.of(context)?.settings.name == SplashScreen().toString();
+    bool willPopToSplashScreen = !Navigator.canPop(context) || 
+      ModalRoute.of(context)?.settings.name == SplashScreen().toString();
+
+    // 如果是 SplashScreen，直接退出应用
+    if (isSplashScreen || willPopToSplashScreen) {
+      try {
+        SystemNavigator.pop();  // 尝试退出应用
+      } catch (e) {
+        LogUtil.e('退出应用错误: $e');
+      }
+      return true;  // 退出应用
+    }
+
+    // 获取当前的屏幕方向
+    var initialOrientation = MediaQuery.of(context).orientation;
+
+    // 延迟检测屏幕方向是否改变
+    await Future.delayed(Duration(milliseconds: screenCheckDelay));
+
+    // 再次获取屏幕方向
+    var currentOrientation = MediaQuery.of(context).orientation;
+
+    // 如果屏幕方向没有改变且不是 SplashScreen，执行默认行为
+    if (currentOrientation == initialOrientation) {
+      return !Navigator.canPop(context);  // 退出应用或返回上一页
+    }
+
+    return false;  // 阻止返回
   }
 
   @override
   Widget build(BuildContext context) {
     // 获取当前语言设置
-    final languageProvider = Provider.of<LanguageProvider>(context);  
+    final languageProvider = Provider.of<LanguageProvider>(context);
 
     // 使用 Selector 来监听主题相关的状态，并根据字体和文本缩放比例进行更新
     return Selector<ThemeProvider, ({String fontFamily, double textScaleFactor})>(
@@ -128,23 +163,24 @@ class _MyAppState extends State<MyApp> {
         // 返回 MaterialApp，配置应用的主题、语言、路由等
         return MaterialApp(
           title: 'ITVAPP LIVETV',  // 应用标题
-          
+
           // 设置应用的主题，包括亮度、颜色方案、字体和其他 UI 样式
           theme: ThemeData(
-              brightness: Brightness.dark,  // 使用暗色主题
-              fontFamily: fontFamily,  // 设置字体
-              colorScheme: ColorScheme.fromSeed(seedColor: Colors.redAccent, brightness: Brightness.dark),  // 基于种子颜色生成色系
-              scaffoldBackgroundColor: Colors.black,  // 背景色设为黑色
-              appBarTheme: const AppBarTheme(
-                backgroundColor: Colors.black,  // AppBar 背景色
-                foregroundColor: Colors.white,  // AppBar 文字颜色
-                elevation: 0,  // 移除阴影
-                centerTitle: true,  // 标题居中
-              ),
-              useMaterial3: true),  // 使用 Material Design 3
+            brightness: Brightness.dark,  // 使用暗色主题
+            fontFamily: fontFamily,  // 设置字体
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.redAccent, brightness: Brightness.dark),  // 基于种子颜色生成色系
+            scaffoldBackgroundColor: Colors.black,  // 背景色设为黑色
+            appBarTheme: const AppBarTheme(
+              backgroundColor: Colors.black,  // AppBar 背景色
+              foregroundColor: Colors.white,  // AppBar 文字颜色
+              elevation: 0,  // 移除阴影
+              centerTitle: true,  // 标题居中
+            ),
+            useMaterial3: true,  // 使用 Material Design 3
+          ),
 
           // 设置应用的语言
-          locale: languageProvider.currentLocale,  
+          locale: languageProvider.currentLocale,
 
           // 定义路由配置
           routes: {
@@ -162,7 +198,7 @@ class _MyAppState extends State<MyApp> {
             GlobalCupertinoLocalizations.delegate,
             GlobalWidgetsLocalizations.delegate
           ],
-          
+
           // 支持的语言列表
           supportedLocales: S.delegate.supportedLocales,
 
@@ -170,7 +206,7 @@ class _MyAppState extends State<MyApp> {
           localeResolutionCallback: (locale, supportedLocales) {
             if (locale != null) {
               // 处理中文的特殊区域
-              if (locale.languageCode == 'zh' && 
+              if (locale.languageCode == 'zh' &&
                   (locale.countryCode == 'TW' || locale.countryCode == 'HK' || locale.countryCode == 'MO')) {
                 return const Locale('zh', 'TW');  // 繁体中文
               }
@@ -180,9 +216,9 @@ class _MyAppState extends State<MyApp> {
               }
               // 匹配合适的语言和国家代码
               return supportedLocales.firstWhere(
-                (supportedLocale) => 
-                  supportedLocale.languageCode == locale.languageCode && 
-                  (supportedLocale.countryCode == locale.countryCode || supportedLocale.countryCode == null),
+                (supportedLocale) =>
+                    supportedLocale.languageCode == locale.languageCode &&
+                    (supportedLocale.countryCode == locale.countryCode || supportedLocale.countryCode == null),
                 orElse: () => supportedLocales.first,
               );
             }
