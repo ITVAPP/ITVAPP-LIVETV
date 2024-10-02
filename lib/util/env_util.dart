50edced9ed0b9876e0e35cd6a792b7c648fe5daa
电视宝 www.itvapp.net <service@itvapp.net>
2024-10-03 03:49:22 +0800

    Update env_util.dart

--- lib/util/env_util.dart
+++ lib/util/env_util.dart
@@ -1,6 +1,8 @@
 import 'dart:io'; 
-import 'dart:ui';  // 导入 dart 的 UI 库，用于获取系统语言环境
-import 'package:flutter/services.dart';  // 用于调用 Android 平台的原生方法
+import 'dart:ui';  
+import 'package:flutter/services.dart';  
+import 'package:itvapp_live_tv/util/log_util.dart';
+import 'package:device_info_plus/device_info_plus.dart'; // 引入 device_info_plus 包
 
 // EnvUtil 类用于提供设备、环境和语言的检测以及不同资源地址的获取
 class EnvUtil {
@@ -10,10 +12,28 @@ class EnvUtil {
   static Future<bool> isTV() async {
     try {
       final bool isTV = await _channel.invokeMethod('isTV'); // 通过 Platform Channel 调用 Android 原生 isTV 方法
-      return isTV;
+      if (isTV) return true; // 如果原生判断为 TV，直接返回 true
+
+      // 使用 device_info_plus 获取设备信息
+      final deviceInfo = DeviceInfoPlugin();
+      if (Platform.isAndroid) {
+        final androidInfo = await deviceInfo.androidInfo;
+        // 判断品牌和型号
+        return androidInfo.isPhysicalDevice && 
+               (androidInfo.brand == 'Android' && 
+                androidInfo.model.contains('TV'));
+      } else if (Platform.isIOS) {
+        final iosInfo = await deviceInfo.iosInfo;
+        // 检查设备型号是否为 Apple TV
+        return iosInfo.model.contains('Apple TV');
+      }
+      return false; // 对于其他平台默认返回 false
     } on PlatformException catch (e) {
-      print("Failed to check if device is TV: '${e.message}'.");
+      LogUtil.e('检测设备发生错误: ${e.message}');
       return false; // 出现异常时返回 false，默认不是电视
+    } catch (e) {
+      LogUtil.e('检测设备发生未知错误: ${e.toString()}');
+      return false; // 处理其他异常，默认返回 false
     }
   }
 
