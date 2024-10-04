1ac83493eca1c7bd8a6d6f4db36467087b003008
电视宝 www.itvapp.net <service@itvapp.net>
2024-10-04 23:12:54 +0800

    Update config.dart

--- lib/config.dart
+++ lib/config.dart
@@ -1,6 +1,9 @@
 class Config {
   /// 当前应用版本
   static const String version = '1.5.8';
+
+  /// 当前应用域名（使用某些流量统计API时可以使用）
+  static const String hostname = 'livetv.itvapp.net';
   
   /// 定义收藏列表的本地缓存键
   static const String favoriteCacheKey = 'favorite_m3u_cache';
