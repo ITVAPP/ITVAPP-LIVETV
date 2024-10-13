dc09eae9b58d897223382b2bc25ff9ae0b6b0660
电视宝 www.itvapp.net <service@itvapp.net>
2024-10-13 16:55:21 +0800

    Update tv_key_navigation.dart

--- lib/tv/tv_key_navigation.dart
+++ lib/tv/tv_key_navigation.dart
@@ -484,7 +484,7 @@ void _triggerButtonAction() {
       while (element != null) {
         final widget = element.widget;
         tryInvokeCallback(widget);
-        element = element.findAncestorRenderObjectOfType<Element>();
+        element = element.findAncestorRenderObjectOfType();
       }
 
       // 提示找到的操作信息
