270127ace985a061f8f61514776a86160828770e
电视宝 www.itvapp.net <service@itvapp.net>
2024-10-17 16:44:48 +0800

    Update channel_util.dart

--- lib/util/channel_util.dart
+++ lib/util/channel_util.dart
@@ -6,83 +6,97 @@ import 'package:itvapp_live_tv/util/log_util.dart';
 import 'package:itvapp_live_tv/tv/tv_key_navigation.dart';
 import '../generated/l10n.dart';
 
-/// 显示底部弹出框选择不同的视频源
-Future<int?> changeChannelSources(
-  BuildContext context, 
-  List<String>? sources, 
-  int currentSourceIndex,
-) async {
-  // 如果 sources 为空或未找到有效的视频源，记录日志并返回 null
-  if (sources == null || sources.isEmpty) {
-    LogUtil.e('未找到有效的视频源');
-    return null;
+class ChangeChannelSourcesWidget extends StatefulWidget {
+  final List<String>? sources;
+  final int currentSourceIndex;
+
+  ChangeChannelSourcesWidget({
+    required this.sources,
+    required this.currentSourceIndex,
+  });
+
+  @override
+  _ChangeChannelSourcesWidgetState createState() =>
+      _ChangeChannelSourcesWidgetState();
+}
+
+class _ChangeChannelSourcesWidgetState
+    extends State<ChangeChannelSourcesWidget> {
+  late List<FocusNode> focusNodes;
+
+  @override
+  void initState() {
+    super.initState();
+    // 为每个 FocusNode 添加监听器
+    focusNodes = List.generate(widget.sources?.length ?? 0, (index) {
+      FocusNode focusNode = FocusNode();
+      focusNode.addListener(() {
+        setState(() {}); // 焦点变化时触发 UI 重绘
+      });
+      return focusNode;
+    });
   }
 
-  // 创建一次性的 FocusNode 列表，无需为每个 FocusNode 添加监听器
-  final List<FocusNode> focusNodes = List.generate(sources.length, (index) => FocusNode());
-
-  // 定义选中与未选中的颜色变量
-  final Color selectedColor = const Color(0xFFEB144C); // 选中时背景颜色
-  final Color unselectedColor = const Color(0xFFDFA02A); // 未选中时背景颜色
-
-  try {
-    // 计算屏幕的方向、宽度和底部间距
-    var orientation = MediaQuery.of(context).orientation;
-    final widthFactor = orientation == Orientation.landscape ? 0.68 : 0.88;
-    final bottomOffset = orientation == Orientation.landscape ? 88.0 : 68.0; // 横屏88.0，竖屏68.0
-
-    // 使用 showModalBottomSheet 来创建一个从底部弹出的弹窗
-    final selectedIndex = await showModalBottomSheet<int>(
-      context: context,
-      isScrollControlled: true, // 允许高度根据内容调整
-      backgroundColor: Colors.transparent, // 背景透明，便于自定义样式
-      builder: (BuildContext context) {
-        return TvKeyNavigation( // 包裹整个弹窗，确保焦点管理覆盖所有内容
-          focusNodes: focusNodes,
-          initialIndex: currentSourceIndex,
-          child: Padding(
-            // 设置弹窗和屏幕底部的距离
-            padding: EdgeInsets.only(bottom: bottomOffset),
-            child: Container(
-              width: MediaQuery.of(context).size.width * widthFactor, // 根据屏幕方向设置宽度
-              padding: EdgeInsets.all(10), // 内边距设置
-              decoration: BoxDecoration(
-                color: Colors.black54, // 设置弹窗背景颜色
-                borderRadius: BorderRadius.vertical(top: Radius.circular(16)), // 仅上边缘圆角
-              ),
-              child: ConstrainedBox(
-                constraints: BoxConstraints(
-                  maxWidth: MediaQuery.of(context).size.width * widthFactor, // 限制弹窗最大宽度
-                  maxHeight: MediaQuery.of(context).size.height * 0.7, // 限制弹窗最大高度为屏幕的70%
-                ),
-                child: buildSourceButtons(context, sources, currentSourceIndex, focusNodes, selectedColor, unselectedColor), // 传递颜色
-              ),
-            ),
-          ),
-        );
-      },
-    );
-
-    // 返回用户选择的索引
-    return selectedIndex;
-  } catch (modalError, modalStackTrace) {
-    LogUtil.logError('弹出窗口时出错', modalError, modalStackTrace);
-    return null;
-  } finally {
+  @override
+  void dispose() {
+    // 销毁 FocusNode
     for (var node in focusNodes) {
       node.dispose();
     }
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final Color selectedColor = const Color(0xFFEB144C); // 选中时背景颜色
+    final Color unselectedColor = const Color(0xFFDFA02A); // 未选中时背景颜色
+
+    // 如果 sources 为空或未找到有效的视频源，记录日志并返回空
+    if (widget.sources == null || widget.sources!.isEmpty) {
+      LogUtil.e('未找到有效的视频源');
+      return Container();
+    }
+
+    // 显示底部弹出框选择不同的视频源
+    return TvKeyNavigation(
+      focusNodes: focusNodes,
+      initialIndex: widget.currentSourceIndex,
+      child: Padding(
+        padding: EdgeInsets.only(bottom: 68.0), // 底部间距
+        child: Container(
+          padding: EdgeInsets.all(10), // 内边距设置
+          decoration: BoxDecoration(
+            color: Colors.black54, // 背景色
+            borderRadius: BorderRadius.vertical(top: Radius.circular(16)),
+          ),
+          child: ConstrainedBox(
+            constraints: BoxConstraints(
+              maxWidth: MediaQuery.of(context).size.width * 0.88, // 宽度限制
+              maxHeight: MediaQuery.of(context).size.height * 0.7, // 高度限制
+            ),
+            child: buildSourceButtons(
+              context,
+              widget.sources!,
+              widget.currentSourceIndex,
+              focusNodes,
+              selectedColor,
+              unselectedColor,
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
 
 /// 构建视频源按钮组
 Widget buildSourceButtons(
-  BuildContext context, 
-  List<String> sources, 
+  BuildContext context,
+  List<String> sources,
   int currentSourceIndex,
-  List<FocusNode> focusNodes, // 传递预先创建的焦点节点
-  Color selectedColor, // 接收选中时的颜色
-  Color unselectedColor, // 接收未选中时的颜色
+  List<FocusNode> focusNodes,
+  Color selectedColor,
+  Color unselectedColor,
 ) {
   return Wrap(
     spacing: 8, // 按钮之间的水平间距
@@ -122,9 +136,9 @@ Widget buildSourceButtons(
 
 /// 获取按钮样式
 ButtonStyle getButtonStyle({
-  required bool isSelected, 
-  required bool isFocused, 
-  required Color selectedColor, 
+  required bool isSelected,
+  required bool isFocused,
+  required Color selectedColor,
   required Color unselectedColor,
 }) {
   Color backgroundColor = isSelected
