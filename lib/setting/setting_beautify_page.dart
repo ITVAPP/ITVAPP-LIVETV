f3a78e4959a4acee9b9fb9ecf86614f3f05b94ea
电视宝 www.itvapp.net <service@itvapp.net>
2024-10-20 23:47:54 +0800

    Update setting_beautify_page.dart

--- lib/setting/setting_beautify_page.dart
+++ lib/setting/setting_beautify_page.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:itvapp_live_tv/provider/theme_provider.dart';
 import 'package:itvapp_live_tv/util/log_util.dart';
+import 'package:flutter/services.dart';
 import '../generated/l10n.dart';
+import 'tv_key_navigation.dart'; // 引入 TvKeyNavigation 组件
 
 class SettingBeautifyPage extends StatefulWidget {
   const SettingBeautifyPage({super.key});
@@ -12,20 +14,28 @@ class SettingBeautifyPage extends StatefulWidget {
 }
 
 class _SettingBeautifyPageState extends State<SettingBeautifyPage> {
-  // 焦点节点，用于TV端焦点管理
-  final FocusNode _bingFocusNode = FocusNode();
 
+  final Color selectedColor = const Color(0xFFEB144C); // 选中时背景颜色
+  final Color unselectedColor = const Color(0xFFDFA02A); // 未选中时背景颜色
+
+  // 设置焦点节点
+  final List<FocusNode> _focusNodes = List.generate(1, (index) => FocusNode());
+  
   @override
   void initState() {
     super.initState();
-    // 页面加载时，将焦点默认设置到第一个可用组件
-    _bingFocusNode.requestFocus();
+    // 监听焦点变化，焦点变化时触发重绘
+    for (var focusNode in _focusNodes) {
+      focusNode.addListener(() {
+        setState(() {}); // 焦点变化时触发setState来重绘UI
+      });
+    }
   }
-
+  
   @override
   void dispose() {
     // 释放焦点资源
-    _bingFocusNode.dispose();
+    _focusNodes.forEach((node) => node.dispose()); // 释放所有焦点节点
     super.dispose();
   }
 
@@ -41,59 +51,76 @@ class _SettingBeautifyPageState extends State<SettingBeautifyPage> {
       // 设置最大容器宽度为 580，适用于大屏幕设备
       double maxContainerWidth = 580;
 
-      return Scaffold(
-        backgroundColor: isTV ? const Color(0xFF1E2022) : null, // 在 TV 模式下设置背景颜色
-        appBar: AppBar(
-          title: Text(
-            S.of(context).backgroundImageTitle,  // AppBar 标题
-            style: const TextStyle(
-              fontSize: 22, // 设置字号
-              fontWeight: FontWeight.bold, // 设置加粗
+      // 使用 FocusScope 包裹 TvKeyNavigation，确保焦点正确管理
+      return FocusScope(
+        child: TvKeyNavigation(
+          focusNodes: _focusNodes, // 传入焦点节点列表
+          isHorizontalGroup: true, // 启用横向分组
+          initialIndex: 0, // 设置初始焦点索引为 0
+          isFrame: isTV ? true : false, // TV 模式下启用框架模式
+          frameType: isTV ? "child" : null, // TV 模式下设置为子页面
+          child: Scaffold(
+            backgroundColor: isTV ? const Color(0xFF1E2022) : null, // TV 模式下设置背景颜色
+            appBar: AppBar(
+              title: Text(
+                S.of(context).backgroundImageTitle, // AppBar 标题
+                style: const TextStyle(
+                  fontSize: 22, // 设置字号
+                  fontWeight: FontWeight.bold, // 设置加粗
+                ),
+              ),
+              backgroundColor: isTV ? const Color(0xFF1E2022) : null, // TV 模式下设置 AppBar 颜色
+              leading: isTV ? const SizedBox.shrink() : null, // 如果是 TV 模式，隐藏返回按钮
             ),
-          ),
-          backgroundColor: isTV ? const Color(0xFF1E2022) : null, // TV 模式下设置 AppBar 颜色
-          leading: isTV ? const SizedBox.shrink() : null, // 如果是 TV 模式，隐藏返回按钮
-        ),
-        body: Align(
-          alignment: Alignment.center, // 内容居中显示
-          child: Container(
-            constraints: BoxConstraints(
-              maxWidth: screenWidth > 580 ? maxContainerWidth : double.infinity, // 限制最大宽度
-            ),
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10), // 增加内边距
-              child: FocusTraversalGroup( // 在 TV 模式下使用焦点组管理方向键切换
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start, // 子组件从左对齐
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 10.0), // 添加垂直间距
-                      child: SwitchListTile(
-                        focusNode: _bingFocusNode, // 将焦点节点绑定到该组件
-                        title: Text(
-                          S.of(context).dailyBing,  // 使用 S.of(context).dailyBing 返回的字符串
-                          style: TextStyle(
-                            fontSize: 18, // 设置字体大小为 18
-                            fontWeight: FontWeight.bold,
+            body: Align(
+              alignment: Alignment.center, // 内容居中显示
+              child: Container(
+                constraints: BoxConstraints(
+                  maxWidth: screenWidth > 580 ? maxContainerWidth : double.infinity, // 限制最大宽度
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10), // 增加内边距
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start, // 子组件从左对齐
+                    children: [
+                      Group( // 使用 Group 包裹分组 0
+                        groupIndex: 0, // 分组索引
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 10.0), // 添加垂直间距
+                            child: FocusableItem( // 使用 FocusableItem 包裹焦点节点
+                              focusNode: _focusNodes[0], // 将焦点节点绑定到该组件
+                              child: SwitchListTile(
+                                title: Text(
+                                  S.of(context).dailyBing, 
+                                  style: TextStyle(
+                                    fontSize: 18, // 设置字体大小为 18
+                                    fontWeight: FontWeight.bold, // 设置加粗
+                                  ),
+                                ),
+                                subtitle: Text(
+                                  S.of(context).backgroundImageDescription, // 提示信息
+                                  style: TextStyle(fontSize: 18), // 设置字体大小
+                                ),
+                                value: context.watch<ThemeProvider>().isBingBg, // 读取 Bing 背景状态
+                                onChanged: (value) {
+                                  LogUtil.safeExecute(() {
+                                    context.read<ThemeProvider>().setBingBg(value); // 设置 Bing 背景状态
+                                  }, '设置每日Bing背景时发生错误');
+                                },
+                                activeColor: Colors.white, // 滑块的颜色
+                                activeTrackColor: _focusNodes[0].hasFocus
+                                   ? selectedColor // 聚焦时颜色变暗
+                                   : selectedColor, // 启动时背景颜色
+                                inactiveThumbColor: Colors.white, // 关闭时滑块的颜色
+                                inactiveTrackColor: Colors.grey, // 关闭时轨道的背景颜色
+                              ),
+                            ),
                           ),
-                        ), // 设置标题加粗
-                        subtitle: Text(
-                          S.of(context).backgroundImageDescription, // 提示信息
-                          style: TextStyle(fontSize: 18), // 设置字体大小
-                        ),
-                        value: context.watch<ThemeProvider>().isBingBg, // 读取 Bing 背景状态
-                        onChanged: (value) {
-                          LogUtil.safeExecute(() {
-                            context.read<ThemeProvider>().setBingBg(value); // 设置 Bing 背景状态
-                          }, '设置每日Bing背景时发生错误');
-                        },
-                        activeColor: Colors.white, // 启用时滑块颜色
-                        activeTrackColor: const Color(0xFFEB144C), // 启用时轨道颜色
-                        inactiveThumbColor: Colors.white, // 关闭时滑块颜色
-                        inactiveTrackColor: Colors.grey, // 关闭时轨道颜色
+                        ],
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
             ),
