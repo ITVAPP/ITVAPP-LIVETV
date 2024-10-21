c67e2b6c0f170a86dc34580d9941836ef1106eb9
电视宝 www.itvapp.net <service@itvapp.net>
2024-10-21 10:23:04 +0800

    Update setting_font_page.dart

--- lib/setting/setting_font_page.dart
+++ lib/setting/setting_font_page.dart
@@ -17,14 +17,14 @@ class _SettingFontPageState extends State<SettingFontPage> {
   final _languages = ['English', '简体中文', '正體中文']; // 语言显示名称
   final _languageCodes = ['en', 'zh_CN', 'zh_TW']; // 语言代码
 
-  // 提取统一的按钮样式
-  RoundedRectangleBorder get _buttonShape => RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(16), // 设置圆角
-      );
+  // 统一的圆角样式
+  final _buttonShape = RoundedRectangleBorder(
+    borderRadius: BorderRadius.circular(16), // 设置圆角
+  );
 
-  // 定义选中和未选中的颜色
-  static const _selectedColor = Color(0xFFEB144C); // 选中时颜色
-  static const _unselectedColor = Color(0xFFDFA02A); // 未选中时颜色
+  // 按钮颜色更新
+  final _selectedColor = const Color(0xFFEB144C); // 选中时颜色
+  final _unselectedColor = const Color(0xFFDFA02A); // 未选中时颜色
 
   // 焦点节点列表（按顺序分配给字体和语言选择按钮）
   final List<FocusNode> _focusNodes = List.generate(8, (index) => FocusNode());
@@ -55,20 +55,15 @@ class _SettingFontPageState extends State<SettingFontPage> {
 
   @override
   Widget build(BuildContext context) {
-    // 缓存 ThemeProvider 和 LanguageProvider，减少重复调用 context.watch()
-    final themeProvider = context.watch<ThemeProvider>();
-    final languageProvider = context.watch<LanguageProvider>();
-
     var screenWidth = MediaQuery.of(context).size.width;
-    bool isTV = themeProvider.isTV; // 获取 TV 模式状态
+    bool isTV = context.watch<ThemeProvider>().isTV;
     double maxContainerWidth = 580;
 
     return FocusScope(
       child: TvKeyNavigation(
         focusNodes: _focusNodes,
-        isHorizontalGroup: true, // 启用横向分组
         initialIndex: 0, // 设置初始焦点索引为 0
-        isFrame: isTV, // TV 模式下启用框架模式
+        isFrame: isTV ? true : false, // TV 模式下启用框架模式
         frameType: isTV ? "child" : null, // TV 模式下设置为子页面
         child: Scaffold(
           backgroundColor: isTV ? const Color(0xFF1E2022) : null, // TV模式下背景颜色
@@ -92,10 +87,147 @@ class _SettingFontPageState extends State<SettingFontPage> {
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     // 字体大小选择部分
-                    _buildFontSizeSelection(themeProvider),
+                    Padding(
+                      padding: const EdgeInsets.all(15),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            S.of(context).fontSizeTitle, // 字体大小标题
+                            style: const TextStyle(
+                                fontSize: 20, fontWeight: FontWeight.bold),
+                          ),
+                          const SizedBox(height: 10), // 间距
+                          Group(
+                            groupIndex: 0, // 分组 0：字体大小
+                            children: [
+                              Wrap(
+                                spacing: 5,
+                                runSpacing: 8, // 按钮排列方式
+                                children: List.generate(
+                                  _fontScales.length,
+                                  (index) => FocusableItem(
+                                    focusNode: _focusNodes[index], // 分配焦点节点
+                                    child: ChoiceChip(
+                                      label: Text(
+                                        '${_fontScales[index]}', // 显示字体大小
+                                        style: TextStyle(
+                                          fontSize: 18,
+                                          color: Colors.white,
+                                          fontWeight: context
+                                                      .watch<ThemeProvider>()
+                                                      .textScaleFactor == _fontScales[index]
+                                              ? FontWeight.bold
+                                              : FontWeight.normal,
+                                        ),
+                                      ),
+                                      selected: context
+                                              .watch<ThemeProvider>()
+                                              .textScaleFactor == _fontScales[index],
+                                      onSelected: (bool selected) {
+                                        context
+                                            .read<ThemeProvider>()
+                                            .setTextScale(_fontScales[index]); // 更新字体大小
+                                      },
+                                      selectedColor: _selectedColor,
+                                      backgroundColor: _focusNodes[index].hasFocus
+                                          ? darkenColor(_unselectedColor)
+                                          : _unselectedColor,
+                                      shape: _buttonShape,
+                                      padding: const EdgeInsets.symmetric(
+                                          horizontal: 5, vertical: 6),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
                     const SizedBox(height: 12), // 间距
+
                     // 语言选择部分
-                    _buildLanguageSelection(languageProvider),
+                    Padding(
+                      padding: const EdgeInsets.all(15), // 外边距
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            S.of(context).languageSelection, // 语言选择标题
+                            style: const TextStyle(
+                                fontSize: 20, fontWeight: FontWeight.bold),
+                          ),
+                          const SizedBox(height: 6), // 间距
+                          Column(
+                            children: List.generate(
+                              _languages.length,
+                              (index) => Group(
+                                groupIndex: index + 1, // 分组索引递增
+                                children: [
+                                  FocusableItem(
+                                    focusNode: _focusNodes[index + 5], // 分配焦点节点
+                                    child: Padding(
+                                      padding: const EdgeInsets.only(bottom: 8.0), // 下边距
+                                      child: Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceBetween,
+                                        children: [
+                                          Text(
+                                            _languages[index],
+                                            style: const TextStyle(fontSize: 18),
+                                          ),
+                                          ChoiceChip(
+                                            label: Text(
+                                              context
+                                                          .watch<LanguageProvider>()
+                                                          .currentLocale
+                                                          .toString() ==
+                                                      _languageCodes[index]
+                                                  ? S.of(context).inUse
+                                                  : S.of(context).use,
+                                              style: TextStyle(
+                                                fontSize: 18,
+                                                color: Colors.white,
+                                                fontWeight: context
+                                                            .watch<LanguageProvider>()
+                                                            .currentLocale
+                                                            .toString() ==
+                                                        _languageCodes[index]
+                                                    ? FontWeight.bold
+                                                    : FontWeight.normal,
+                                              ),
+                                            ),
+                                            selected: context
+                                                    .watch<LanguageProvider>()
+                                                    .currentLocale
+                                                    .toString() ==
+                                                _languageCodes[index],
+                                            onSelected: (bool selected) {
+                                              context
+                                                  .read<LanguageProvider>()
+                                                  .changeLanguage(
+                                                      _languageCodes[index]); // 切换语言
+                                            },
+                                            selectedColor: _selectedColor,
+                                            backgroundColor: _focusNodes[index + 5].hasFocus
+                                                ? darkenColor(_unselectedColor)
+                                                : _unselectedColor,
+                                            shape: _buttonShape,
+                                            padding: const EdgeInsets.symmetric(
+                                                horizontal: 5, vertical: 6),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
                   ],
                 ),
               ),
@@ -103,99 +235,6 @@ class _SettingFontPageState extends State<SettingFontPage> {
           ),
         ),
       ),
-    );
-  }
-
-  // 抽取字体大小选择部分为独立函数，使用 Group 包裹
-  Widget _buildFontSizeSelection(ThemeProvider themeProvider) {
-    return Padding(
-      padding: const EdgeInsets.all(15),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            S.of(context).fontSizeTitle, // 字体大小标题
-            style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-          ),
-          const SizedBox(height: 10), // 间距
-          Group(
-            groupIndex: 0, // 设置 Group 的索引为 0
-            children: List.generate(_fontScales.length, (index) {
-              return FocusableItem(
-                focusNode: _focusNodes[index], // 包裹按钮并分配焦点节点
-                child: _buildChoiceChip(
-                  label: '${_fontScales[index]}',
-                  isSelected: themeProvider.textScaleFactor == _fontScales[index],
-                  onSelected: () => themeProvider.setTextScale(_fontScales[index]),
-                ),
-              );
-            }),
-          ),
-        ],
-      ),
-    );
-  }
-
-  // 抽取语言选择部分为独立函数，每个 FocusableItem 用 Group 包裹
-  Widget _buildLanguageSelection(LanguageProvider languageProvider) {
-    return Padding(
-      padding: const EdgeInsets.all(15), // 外边距
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            S.of(context).languageSelection, // 语言选择标题
-            style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-          ),
-          const SizedBox(height: 6), // 间距
-          Column(
-            children: List.generate(_languages.length, (index) {
-              return Group(
-                groupIndex: index + 1, // 为每个语言选择设置递增的 groupIndex
-                children: [
-                  FocusableItem(
-                    focusNode: _focusNodes[index + 5], // 包裹按钮并分配焦点节点
-                    child: _buildChoiceChip(
-                      label: languageProvider.currentLocale.toString() ==
-                              _languageCodes[index]
-                          ? S.of(context).inUse
-                          : S.of(context).use,
-                      isSelected: languageProvider.currentLocale.toString() ==
-                          _languageCodes[index],
-                      onSelected: () =>
-                          languageProvider.changeLanguage(_languageCodes[index]),
-                    ),
-                  ),
-                ],
-              );
-            }),
-          ),
-        ],
-      ),
-    );
-  }
-
-  // 提取 ChoiceChip 的构建逻辑，减少重复代码
-  Widget _buildChoiceChip({
-    required String label,
-    required bool isSelected,
-    required VoidCallback onSelected,
-  }) {
-    return ChoiceChip(
-      label: Text(
-        label,
-        style: TextStyle(
-          fontSize: 18,
-          color: Colors.white,
-          fontWeight: isSelected ? FontWeight.bold : FontWeight.normal,
-        ),
-      ),
-      selected: isSelected,
-      onSelected: (_) => onSelected(),
-      selectedColor: _selectedColor,
-      backgroundColor: isSelected ? darkenColor(_unselectedColor) : _unselectedColor,
-      shape: _buttonShape,
-      padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 6),
     );
   }
 }
