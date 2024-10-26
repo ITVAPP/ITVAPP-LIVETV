842aebfe62537c57f2f8ba501c90e24b966174d4
电视宝 www.itvapp.net <service@itvapp.net>
2024-10-26 16:05:21 +0800

    Update tv_key_navigation.dart

--- lib/tv/tv_key_navigation.dart
+++ lib/tv/tv_key_navigation.dart
@@ -266,6 +266,13 @@ void _manageDebugOverlay({String? message}) {
     }
   }
 
+  /// 刷新 Group 缓存的焦点信息
+void refreshGroupFocusCache() {
+  _groupFocusCache.clear();
+  _cacheGroupFocusNodes();  // 重新缓存当前的焦点节点信息
+  _manageDebugOverlay(message: '已刷新分组焦点缓存');
+}
+
   /// 缓存 Group 的焦点信息
 void _cacheGroupFocusNodes() {
   _groupFocusCache.clear();  // 清空缓存
