6ec6005da8286a18a6a4fedceb055c4c1908e610
电视宝 www.itvapp.net <service@itvapp.net>
2024-11-20 01:43:35 +0800

    Update table_video_widget.dart

--- lib/table_video_widget.dart
+++ lib/table_video_widget.dart
@@ -146,9 +146,6 @@ class _TableVideoWidgetState extends State<TableVideoWidget> with WindowListener
     LogUtil.safeExecute(() {
       if (!EnvUtil.isMobile) windowManager.removeListener(this);
     }, '移除窗口监听器发生错误');
-   if (widget.isAudio && widget.controller != null) {
-     widget.controller!.removeEventsListener(_videoListener);
-   }
     super.dispose();
   }
 
