27e1657b54c585cc3aee2554b4df7a669f8885bf
电视宝 www.itvapp.net <service@itvapp.net>
2024-11-20 03:08:57 +0800

    Update download_provider.dart

--- lib/provider/download_provider.dart
+++ lib/provider/download_provider.dart
@@ -1,8 +1,8 @@
 import 'package:apk_installer/apk_installer.dart';
+import 'package:itvapp_live_tv/util/log_util.dart';
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
-import 'package:itvapp_live_tv/util/log_util.dart';
-import 'package:itvapp_live_tv/util/http_util.dart';
+import '../util/http_util.dart';
 
 class DownloadProvider extends ChangeNotifier {
   static final DownloadProvider _instance = DownloadProvider._internal();
@@ -24,49 +24,45 @@ class DownloadProvider extends ChangeNotifier {
     }
 
     try {
-      await LogUtil.safeExecute(() async {
-        _isDownloading = true;
-        _currentUrl = url;
-        _progress = 0.0;
+      _isDownloading = true;
+      _currentUrl = url;
+      _progress = 0.0;
+      notifyListeners();
+      LogUtil.v('开始下载 APK 文件: $url');
+
+      final dir = await getTemporaryDirectory();
+      final savePath = '${dir.path}/apk/${url.split('/').last}';
+      LogUtil.v('APK 保存路径: $savePath');
+
+      final downloadCode = await HttpUtil().downloadFile(
+        url,
+        savePath,
+        progressCallback: (double currentProgress) {
+          try {
+            _progress = currentProgress;
+            notifyListeners();
+          } catch (e, stackTrace) {
+            LogUtil.logError('更新下载进度时发生错误', e, stackTrace);
+          }
+        },
+      );
+
+      if (downloadCode == 200) {
+        LogUtil.v('APK 文件下载完成，开始安装: $savePath');
+        await ApkInstaller.installApk(filePath: savePath);
+      } else {
+        LogUtil.e('下载 APK 文件失败，错误码: $downloadCode');
+        _isDownloading = false;
         notifyListeners();
-        LogUtil.v('开始下载 APK 文件: $url');
-
-        final dir = await getTemporaryDirectory();
-        final savePath = '${dir.path}/apk/${url.split('/').last}';
-        LogUtil.v('APK 保存路径: $savePath');
-
-        final code = await HttpUtil().downloadFile(
-          url,
-          savePath,
-          progressCallback: (double currentProgress) {
-            try {
-              _progress = currentProgress;
-              notifyListeners();
-            } catch (e, stackTrace) {
-              LogUtil.logError('更新下载进度时发生错误', e, stackTrace);
-            }
-          },
-        );
-
-        if (code == 200) {
-          LogUtil.v('APK 文件下载完成，开始安装: $savePath');
-          await ApkInstaller.installApk(filePath: savePath);
-        } else {
-          LogUtil.e('下载 APK 文件失败，错误码: $code');
-          _isDownloading = false;
-          notifyListeners();
-        }
-      }, '下载过程中发生错误');
+      }
     } catch (e, stackTrace) {
       LogUtil.logError('下载或安装 APK 时发生错误', e, stackTrace);
       _isDownloading = false;
       notifyListeners();
     } finally {
-      if (code == 200) {
-        _isDownloading = false;
-        _currentUrl = null;
-        notifyListeners();
-      }
+      _isDownloading = false;
+      _currentUrl = null;
+      notifyListeners();
     }
   }
 }
