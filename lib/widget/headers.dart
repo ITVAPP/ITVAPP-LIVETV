b42ec6a4cc3e63b1085b7aa68cd6a3d7020c61c9
电视宝 www.itvapp.net <service@itvapp.net>
2025-01-07 18:09:26 +0800

    Update headers.dart

--- lib/widget/headers.dart
+++ lib/widget/headers.dart
@@ -10,7 +10,7 @@ class HeadersConfig {
  
  /// 格式: domain1|referer1@domain2|referer2
  /// 例如: 'googlevideo|www.youtube.com@example.com|example.org'
- static String rulesString = 'googlevideo|www.youtube.com@tcdn.itouchtv.cn|www.gdtv.cn@lanosso.com|lanzoux.com@wwentua.com|lanzoux.com@btime.com|www.btime.com@kksmg.com|live.kankanews.com@iqilu|v.iqilu.com@cditvcn|www.cditv.cn@candocloud.cn|www.cditv.cn@yntv-api.yntv.cn|www.yntv.cn@api.yntv.ynradio.com|www.ynradio.cn@i0834.cn|www.ls666.com@dzxw.net|www.dzrm.cn@zyrb.com.cn|www.sczytv.com@ningxiahuangheyun.com|www.nxtv.com.cn@quklive.com|www.qukanvideo.com@yuexitv|www.yuexitv.com@ahsxrm|www.ahsxrm.cn@liangtv.cn|tv.gxtv.cn@lcxw.cn|www.lcxw.cn@sxtygdy.com|www.sxtygdy.com@sxrtv.com|www.sxrtv.com';
+ static String rulesString = 'googlevideo|www.youtube.com@tcdn.itouchtv.cn|www.gdtv.cn@lanosso.com|lanzoux.com@wwentua.com|lanzoux.com@btime.com|www.btime.com@kksmg.com|live.kankanews.com@iqilu|v.iqilu.com@cditvcn|www.cditv.cn@candocloud.cn|www.cditv.cn@yntv-api.yntv.cn|www.yntv.cn@api.yntv.ynradio.com|www.ynradio.cn@i0834.cn|www.ls666.com@dzxw.net|www.dzrm.cn@zyrb.com.cn|www.sczytv.com@ningxiahuangheyun.com|www.nxtv.com.cn@quklive.com|www.qukanvideo.com@yuexitv|www.yuexitv.com@ahsxrm|www.ahsxrm.cn@liangtv.cn|tv.gxtv.cn@mobilelive.cn|www.gxtv.cn@lcxw.cn|www.lcxw.cn@sxtygdy.com|www.sxtygdy.com@sxrtv.com|www.sxrtv.com';
 
  /// CORS规则字符串，格式: domain1@domain2@domain3
  static String corsRulesString = 'itvapp.net@file.lcxw.cn@livehhhttps.sxrtv.com@hhplusconsole.sxrtv.com';
@@ -133,21 +133,37 @@ static String? _getRefererByRules(String url) {
         } 
       }
     }
-    
-     final headers = {
-       ..._baseHeaders,
-       'origin': referer,
-       'referer': '$referer/',
-       if (needCors) ...{
-         'host': host,
-         'sec-fetch-mode': 'cors',
-         if (secFetchSite != null) 'sec-fetch-site': secFetchSite,
-       }
-     };
-
-     LogUtil.i('生成主机头：$headers');
-     return headers;
      
+// 山西卫视特定的请求头
+if (referer == 'https://www.sxrtv.com') {   
+  return {
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36',
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'accept-encoding': 'gzip, deflate, br',
+    'accept-language': 'zh-CN,zh;q=0.9,en;q=0.8',
+    'cache-control': 'max-age=0',
+    'sec-ch-ua': '".Not/A)Brand";v="99", "Google Chrome";v="103", "Chromium";v="103"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Windows"',
+    'sec-fetch-dest': 'document',
+    'sec-fetch-mode': 'navigate',
+    'sec-fetch-site': 'none',
+    'sec-fetch-user': '?1',
+    'upgrade-insecure-requests': '1',
+  };
+} else {
+  final headers = {
+    ..._baseHeaders,
+    'origin': referer,
+    'referer': '$referer/',
+    if (needCors) ...{
+      'host': host,
+      'sec-fetch-mode': 'cors',
+      if (secFetchSite != null) 'sec-fetch-site': secFetchSite,
+    }
+  };
+  LogUtil.i('生成主机头：$headers');
+  return headers;
    } catch (e, stackTrace) {
      LogUtil.logError('生成Headers失败，使用默认Headers', e, stackTrace);
      return _baseHeaders;
