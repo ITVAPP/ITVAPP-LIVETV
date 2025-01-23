41fff79a48349fec2b0928ebf8623453e3376475
电视宝 www.itvapp.net <service@itvapp.net>
2025-01-23 08:31:47 +0800

    Update getm3u8.dart

--- lib/util/getm3u8.dart
+++ lib/util/getm3u8.dart
@@ -1694,7 +1694,7 @@ bool _isValidM3U8Url(String url) {
     LogUtil.i('URL整理开始，原始URL: $url');
 
     // 如果以\开头，则去除
-    if (url.length > 0 && url[url.length - 1] == '\') {
+    if (url.length > 0 && url[url.length - 1] == '\\') {
       url = url.substring(0, url.length - 1);
     }
 
