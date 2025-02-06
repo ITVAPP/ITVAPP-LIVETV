834f98ab5835fafa594903df77e4630237305bb3
电视宝 www.itvapp.net <service@itvapp.net>
2025-02-06 10:09:17 +0800

    Update font_util.dart

--- lib/util/font_util.dart
+++ lib/util/font_util.dart
@@ -1,7 +1,9 @@
 import 'dart:io';
 import 'dart:ui';
+
 import 'package:flutter/foundation.dart';
 import 'package:path_provider/path_provider.dart';
+
 import 'http_util.dart';
 import 'log_util.dart';
 
@@ -19,7 +21,7 @@ class FontUtil {
     return '$path/fonts';
   }
 
-  Future<Uint8List?> downloadFont(String url, {bool overwrite = false, ValueChanged<double>? progressCallback, CancelToken? cancelToken}) async {
+  Future<Uint8List?> downloadFont(String url, {bool overwrite = false, ValueChanged<double>? progressCallback}) async {
     final uri = Uri.parse(url);
     final filename = uri.pathSegments.last;
     final dir = await getFontPath();
@@ -28,18 +30,11 @@ class FontUtil {
     if (await file.exists() && !overwrite) {
       return file.readAsBytes();
     }
-
-    // 传递 cancelToken，用于取消下载任务
-    final bytes = await downloadBytes(url, filename, fontPath, progressCallback: progressCallback, cancelToken: cancelToken);
+    final bytes = await downloadBytes(url, filename, fontPath, progressCallback: progressCallback);
     return bytes;
   }
 
-  Future<Uint8List?> downloadBytes(String url, String filename, String savePath, {ValueChanged<double>? progressCallback, CancelToken? cancelToken}) async {
-    if (cancelToken?.isCancelled == true) {
-      LogUtil.i('下载请求已取消');
-      return null;  // 如果请求被取消，直接返回
-    }
-
+  Future<Uint8List?> downloadBytes(String url, String filename, String savePath, {ValueChanged<double>? progressCallback}) async {
     final code = await HttpUtil().downloadFile(url, savePath, progressCallback: progressCallback);
     if (code == 200) {
       return File(savePath).readAsBytes();
@@ -58,8 +53,8 @@ class FontUtil {
     }
   }
 
-  Future<bool> loadFont(String url, String fontFamily, {ValueChanged<double>? progressCallback, CancelToken? cancelToken}) async {
-    final fontByte = await downloadFont(url, progressCallback: progressCallback, cancelToken: cancelToken);
+  Future<bool> loadFont(String url, String fontFamily, {ValueChanged<double>? progressCallback}) async {
+    final fontByte = await downloadFont(url, progressCallback: progressCallback);
     if (fontByte == null) return false;
     try {
       await loadFontFromList(fontByte, fontFamily: fontFamily);
