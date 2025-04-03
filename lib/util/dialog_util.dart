c7be6e87b416a708143fef76e1782bc6d806ab90
电视宝 www.itvapp.net <service@itvapp.net>
2025-04-04 01:24:52 +0800

    Update dialog_util.dart

--- lib/util/dialog_util.dart
+++ lib/util/dialog_util.dart
@@ -334,29 +334,22 @@ class DialogUtil {
     );
   }
 
-  // 封装的内容部分，允许选择和复制功能，添加文字颜色和背景隔离
+  // 封装的内容部分，允许选择和复制功能
   static Widget _buildDialogContent({String? content}) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       mainAxisSize: MainAxisSize.min,
       children: [
-        Container(
-          padding: const EdgeInsets.all(8.0), // 添加内边距
-          color: Colors.black.withOpacity(0.7), // 添加半透明背景隔离渐变
-          child: TextField(
-            controller: TextEditingController(text: content ?? ''),
-            readOnly: true,
-            maxLines: null,
-            textAlign: TextAlign.start,
-            decoration: const InputDecoration(
-              border: InputBorder.none,
-            ),
-            style: const TextStyle(
-              fontSize: 18,
-              color: Colors.white, // 明确指定文字颜色为白色
-            ),
-            enableInteractiveSelection: true,
+        TextField(
+          controller: TextEditingController(text: content ?? ''),
+          readOnly: true,
+          maxLines: null,
+          textAlign: TextAlign.start,
+          decoration: const InputDecoration(
+            border: InputBorder.none,
           ),
+          style: const TextStyle(fontSize: 18),
+          enableInteractiveSelection: true,
         ),
       ],
     );
