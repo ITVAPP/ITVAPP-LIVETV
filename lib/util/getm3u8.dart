6e064edfd17ac9ccf332820f0377ea2021ff066c
电视宝 www.itvapp.net <service@itvapp.net>
2025-04-06 06:51:42 +0800

    回退标记

--- lib/util/getm3u8.dart
+++ lib/util/getm3u8.dart
@@ -8,7 +8,7 @@ import 'package:itvapp_live_tv/util/http_util.dart';
 import 'package:itvapp_live_tv/util/getm3u8diy.dart';
 import 'package:itvapp_live_tv/widget/headers.dart';
 
-/// URL 处理工具类
+/// URL 处理工具类 
 class UrlUtils {
   static const String _protocolPatternStr = r'(?:https?)';
   static final RegExp _escapeRegex = RegExp(r'\\(\|/|")');
