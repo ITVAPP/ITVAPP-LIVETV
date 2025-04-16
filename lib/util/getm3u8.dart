ed6edc5fd33b7b3e36edd6b88b8520c4d8c1cff1
电视宝 www.itvapp.net <service@itvapp.net>
2025-04-16 15:43:08 +0800

    Update getm3u8.dart

--- lib/util/getm3u8.dart
+++ lib/util/getm3u8.dart
@@ -153,7 +153,7 @@ class GetM3U8 {
   );
 
   // 过滤规则字符串
-  static String rulesString = 'ptbtv.com|live.m3u8@setv.sh.cn|programme10_ud@kanwz.net|playlist.m3u8@sxtygdy.com|tytv-hls.sxtygdy.com@tvlive.yntv.cn|chunks_dvr_range@appwuhan.com|playlist.m3u8@hbtv.com.cn/new-|aalook=';
+  static String rulesString = 'ptbtv.com|hd/live@setv.sh.cn|programme10_ud@kanwz.net|playlist.m3u8@sxtygdy.com|tytv-hls.sxtygdy.com@tvlive.yntv.cn|chunks_dvr_range@appwuhan.com|playlist.m3u8@hbtv.com.cn/new-|aalook=';
   static String specialRulesString = 'nctvcloud.com|flv@mydomaint.com|mp4'; // 特殊规则字符串
   static String dynamicKeywordsString = 'jinan@gansu@xizang@sichuan'; // 使用getm3u8diy解析的关键词
   static const String allowedResourcePatternsString = 'r.png?t='; // 允许资源模式字符串
