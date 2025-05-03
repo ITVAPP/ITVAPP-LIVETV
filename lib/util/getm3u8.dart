840c97545fb688de2c71e4417a9a96f0ca188f68
电视宝 www.itvapp.net <service@itvapp.net>
2025-05-03 19:08:05 +0800

    Update getm3u8.dart

--- lib/util/getm3u8.dart
+++ lib/util/getm3u8.dart
@@ -28,7 +28,7 @@ class M3U8Constants {
   static const int defaultSetSize = 50; // 默认集合大小
 
   // 字符串常量
-  static const String rulePatterns = 'iptv345.com|flv?sign=@tcrbs.com|auth_key@xybtv.com|auth_key@aodianyun.com|auth_key@ptbtv.com|hd/live@setv.sh.cn|programme10_ud@kanwz.net|playlist.m3u8@sxtygdy.com|tytv-hls.sxtygdy.com@tvlive.yntv.cn|chunks_dvr_range@appwuhan.com|playlist.m3u8@hbtv.com.cn/new-|aalook='; // M3U8过滤规则模式
+  static const String rulePatterns = '4gtv.tv|master.m3u8@iptv345.com|flv?sign=@tcrbs.com|auth_key@xybtv.com|auth_key@aodianyun.com|auth_key@ptbtv.com|hd/live@setv.sh.cn|programme10_ud@kanwz.net|playlist.m3u8@sxtygdy.com|tytv-hls.sxtygdy.com@tvlive.yntv.cn|chunks_dvr_range@appwuhan.com|playlist.m3u8@hbtv.com.cn/new-|aalook='; // M3U8过滤规则模式
   static const String specialRulePatterns = 'nctvcloud.com|flv@iptv345.com|flv'; // 特殊规则模式
   static const String dynamicKeywords = 'sousuo@jinan@gansu@xizang@sichuan@xishui@yanan@foshan'; // getm3u8diy关键字
   static const String whiteExtensions = 'r.png?t=@www.hljtv.com@guangdianyun.tv'; // 白名单关键字
