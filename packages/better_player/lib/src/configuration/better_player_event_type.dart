0b09a7b12c7b4c10906a25017a2359a82120156c
电视宝 www.itvapp.net <service@itvapp.net>
2025-06-18 15:29:42 +0800

    Update better_player_event_type.dart

--- packages/better_player/lib/src/configuration/better_player_event_type.dart
+++ packages/better_player/lib/src/configuration/better_player_event_type.dart
@@ -26,15 +26,3 @@ enum BetterPlayerEventType {
   bufferingEnd,
   changedPlaylistItem,
 }
-
-/// 解码器类型配置
-enum BetterPlayerDecoderType {
-  /// 自动选择（默认）
-  auto,
-
-  /// 硬件解码优先
-  hardwareFirst,
-
-  /// 软件解码优先
-  softwareFirst,
-}
