ac7e5a8b091ce29b34a17d2b5096fc7d746c8ad7
电视宝 www.itvapp.net <service@itvapp.net>
2025-06-18 15:53:32 +0800

    Update video_player.dart

--- packages/better_player/lib/src/video_player/video_player.dart
+++ packages/better_player/lib/src/video_player/video_player.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'dart:io';
 import 'package:better_player/src/configuration/better_player_buffering_configuration.dart';
+import 'package:better_player/src/configuration/better_player_data_source_type.dart';
 import 'package:better_player/src/video_player/video_player_platform_interface.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
