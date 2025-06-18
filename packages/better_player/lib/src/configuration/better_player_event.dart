49bc77a72101df31e07510e989c16e3c8c0f6427
电视宝 www.itvapp.net <service@itvapp.net>
2025-06-18 19:51:31 +0800

    Update better_player_event.dart

--- packages/better_player/lib/src/configuration/better_player_event.dart
+++ packages/better_player/lib/src/configuration/better_player_event.dart
@@ -1,7 +1,6 @@
 import 'package:better_player/src/configuration/better_player_event_type.dart';
 
-///Event that happens in player. It can be used to determine current player state
-///on higher layer.
+/// 播放器状态获取
 class BetterPlayerEvent {
   final BetterPlayerEventType betterPlayerEventType;
   final Map<String, dynamic>? parameters;
