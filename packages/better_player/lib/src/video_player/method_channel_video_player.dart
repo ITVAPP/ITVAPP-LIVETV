eb6249ef1b06f9284a0073e1e255f3dafbc00861
电视宝 www.itvapp.net <service@itvapp.net>
2025-06-18 18:49:17 +0800

    Update method_channel_video_player.dart

--- packages/better_player/lib/src/video_player/method_channel_video_player.dart
+++ packages/better_player/lib/src/video_player/method_channel_video_player.dart
@@ -430,6 +430,13 @@ class MethodChannelVideoPlayer extends VideoPlayerPlatform {
             key: key,
           );
 
+      case 'log': 
+        return VideoEvent(
+          eventType: VideoEventType.log,
+          key: key,
+          logMessage: map['message'] as String?,
+        );
+        
         default:
           return VideoEvent(
             eventType: VideoEventType.unknown,
