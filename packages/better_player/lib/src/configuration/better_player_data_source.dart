69d976e92f62400039710f4749b76de619a02ce7
电视宝 www.itvapp.net <service@itvapp.net>
2025-06-19 21:41:51 +0800

    Update better_player_data_source.dart

--- packages/better_player/lib/src/configuration/better_player_data_source.dart
+++ packages/better_player/lib/src/configuration/better_player_data_source.dart
@@ -137,6 +137,7 @@ class BetterPlayerDataSource {
       drmConfiguration: drmConfiguration,
       placeholder: placeholder,
       bufferingConfiguration: bufferingConfiguration,
+      BetterPlayerDecoderType? preferredDecoderType,
     );
   }
 
