ae40b3a248923fe8dec5abb91cfba66f70c32949
电视宝 www.itvapp.net <service@itvapp.net>
2025-06-24 01:14:58 +0800

    Update iapp_player_theme.dart

--- packages/iapp_player/lib/src/configuration/iapp_player_theme.dart
+++ packages/iapp_player/lib/src/configuration/iapp_player_theme.dart
@@ -1,4 +1,2 @@
-///Representation of possible UI themes in IApp Player. For Android, default
-///theme is [material], for iOS [cupertino]. To use customized theme, set
-///[custom] value.
+/// IApp Player UI 主题枚举，Android 默认 [material]，iOS 默认 [cupertino]，自定义主题使用 [custom]
 enum IAppPlayerTheme { material, cupertino, custom }
