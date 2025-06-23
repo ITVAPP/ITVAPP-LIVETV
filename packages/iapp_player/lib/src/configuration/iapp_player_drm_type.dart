c861eb49085352e9073e8056be374865afaff6d5
电视宝 www.itvapp.net <service@itvapp.net>
2025-06-24 01:17:46 +0800

    Update iapp_player_drm_type.dart

--- packages/iapp_player/lib/src/configuration/iapp_player_drm_type.dart
+++ packages/iapp_player/lib/src/configuration/iapp_player_drm_type.dart
@@ -1,6 +1,6 @@
-///Types of available DRM's
-///token -> supported for both iOS/Android
-///widevine -> supported only for Android
-///fairplay -> suppoted only for iOS
-///clearKey -> supported only for Android
+/// DRM 类型枚举
+/// token: iOS 和 Android 均支持
+/// widevine: 仅 Android 支持
+/// fairplay: 仅 iOS 支持
+/// clearKey: 仅 Android 支持
 enum IAppPlayerDrmType { token, widevine, fairplay, clearKey }
