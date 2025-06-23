38fd0f4b1ca7b72a727d9dbc5266ddf701f71a41
电视宝 www.itvapp.net <service@itvapp.net>
2025-06-24 01:15:43 +0800

    Update iapp_player_notification_configuration.dart

--- packages/iapp_player/lib/src/configuration/iapp_player_notification_configuration.dart
+++ packages/iapp_player/lib/src/configuration/iapp_player_notification_configuration.dart
@@ -1,25 +1,19 @@
-///Configuration of notification which is displayed once user moves app to
-///background.
+/// 应用程序移至后台时显示的通知配置
 class IAppPlayerNotificationConfiguration {
-  ///Is player controls notification enabled
+  /// 是否启用播放器控制通知
   final bool? showNotification;
-
-  ///Title of the given data source, used in controls notification
+  /// 数据源标题，用于控制通知
   final String? title;
-
-  ///Author of the given data source, used in controls notification
+  /// 数据源作者，用于控制通知
   final String? author;
-
-  ///Image of the video, used in controls notification
+  /// 视频封面图片 URL，用于控制通知
   final String? imageUrl;
-
-  ///Name of the notification channel. Used only in Android.
+  /// 通知通道名称，仅 Android 使用
   final String? notificationChannelName;
-
-  ///Name of activity used to open application from notification. Used only
-  ///in Android.
+  /// 从通知打开应用的 Activity 名称，仅 Android 使用
   final String? activityName;
 
+  /// 构造函数，初始化通知配置
   const IAppPlayerNotificationConfiguration({
     this.showNotification,
     this.title,
