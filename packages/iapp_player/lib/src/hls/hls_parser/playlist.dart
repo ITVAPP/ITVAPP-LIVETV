f5b0247f5196e2117d4e548f1616bee284248043
电视宝 www.itvapp.net <service@itvapp.net>
2025-06-23 12:18:08 +0800

    Update playlist.dart

--- packages/iapp_player/lib/src/hls/hls_parser/playlist.dart
+++ packages/iapp_player/lib/src/hls/hls_parser/playlist.dart
@@ -1,3 +1,4 @@
+/// HLS播放列表抽象类
 abstract class HlsPlaylist {
   HlsPlaylist({
     required this.baseUri,
@@ -5,12 +6,12 @@ abstract class HlsPlaylist {
     required this.hasIndependentSegments,
   });
 
-  /// The base uri. Used to resolve relative paths.
+  /// 基础URI，用于解析相对路径
   final String? baseUri;
 
-  /// The list of tags in the playlist.
+  /// 播放列表中的标签列表
   final List<String> tags;
 
-  /// Whether the media is formed of independent segments, as defined by the #EXT-X-INDEPENDENT-SEGMENTS tag.
+  /// 是否由独立分段组成，由#EXT-X-INDEPENDENT-SEGMENTS标签定义
   final bool hasIndependentSegments;
 }
