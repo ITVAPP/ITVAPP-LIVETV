888899ce9734be9c55e75a800a3961277fdce856
电视宝 www.itvapp.net <service@itvapp.net>
2025-06-24 17:17:20 +0800

    Update IAppPlayerTimeUtils.h

--- packages/iapp_player/ios/Classes/IAppPlayerTimeUtils.h
+++ packages/iapp_player/ios/Classes/IAppPlayerTimeUtils.h
@@ -1,17 +1,14 @@
-// Copyright 2017 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 #import <Foundation/Foundation.h>
 #import <AVKit/AVKit.h>
 
 NS_ASSUME_NONNULL_BEGIN
 
+/// 时间转换工具类
 @interface IAppPlayerTimeUtils : NSObject
-
+/// 将 CMTime 转换为毫秒
 + (int64_t) FLTCMTimeToMillis:(CMTime) time;
+/// 将 NSTimeInterval 转换为毫秒
 + (int64_t) FLTNSTimeIntervalToMillis:(NSTimeInterval) interval;
-
 @end
 
 NS_ASSUME_NONNULL_END
